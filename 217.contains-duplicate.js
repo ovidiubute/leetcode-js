f87605a68f2c133590f29eccd348edab09208adf
Ovidiu Bute <ovidiu.bute@gmail.com>
2018-11-03 17:27:30 +0200

    Add solution to 217.

---
+++ 217.contains-duplicate.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+  var seen = {};
+  for (var i = 0; i < nums.length; i++) {
+    if (seen[nums[i]] !== undefined) {
+      return true;
+    } else {
+      seen[nums[i]] = 1;
+    }
+  }
+
+  return false;
+};
