1101d3a43e282c69d7f921c3503e7015bc9e1cd2
Ovidiu Bute <ovidiu.bute@gmail.com>
2018-11-03 18:26:54 +0200

    Add solution to 268.

---
+++ 268.missing-number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+  var min = 0;
+  var s = 0;
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] < min) {
+      min = nums[i];
+    }
+
+    s += nums[i];
+  }
+
+  var sum = ((2 * min + nums.length) * (nums.length + 1)) / 2;
+  return sum - s;
+};
