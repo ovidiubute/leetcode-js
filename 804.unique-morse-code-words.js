52fdffff7c0c5a3b5afb482d1cad3a9c094cc4d4
Ovidiu Bute <ovidiu.bute@gmail.com>
2018-11-03 17:28:53 +0200

    Add solution to 804.

---
+++ 804.unique-morse-code-words.js
@@ -0,0 +1,47 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var uniqueMorseRepresentations = function(words) {
+  const morse = [
+    ".-",
+    "-...",
+    "-.-.",
+    "-..",
+    ".",
+    "..-.",
+    "--.",
+    "....",
+    "..",
+    ".---",
+    "-.-",
+    ".-..",
+    "--",
+    "-.",
+    "---",
+    ".--.",
+    "--.-",
+    ".-.",
+    "...",
+    "-",
+    "..-",
+    "...-",
+    ".--",
+    "-..-",
+    "-.--",
+    "--.."
+  ];
+  const occurences = {};
+  for (let i = 0; i < words.length; i++) {
+    let m = Array.prototype.map
+      .call(words[i], c => morse[c.charCodeAt(0) - 97])
+      .join("");
+    if (occurences[m] !== undefined) {
+      occurences[m]++;
+    } else {
+      occurences[m] = 1;
+    }
+  }
+  console.log(occurences);
+  return Object.keys(occurences).length;
+};
