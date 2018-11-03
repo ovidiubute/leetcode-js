2c74d5c7664ba61f9a790b65aeffe138ec01d11c
Ovidiu Bute <ovidiu.bute@gmail.com>
2018-11-03 22:51:49 +0200

    Add solution to 819.

---
+++ 819.most-common-word.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} paragraph
+ * @param {string[]} banned
+ * @return {string}
+ */
+var mostCommonWord = function(paragraph, banned) {
+  var wordCounts = {};
+  var maxWord;
+  var maxCount = 0;
+
+  paragraph
+    .split(/[!?',;.\ ]/gi)
+    .filter(w => w != "")
+    .map(w => w.toLowerCase())
+    .forEach(word => {
+      if (banned.includes(word)) {
+        return;
+      }
+
+      if (wordCounts[word] !== undefined) {
+        wordCounts[word]++;
+      } else {
+        wordCounts[word] = 1;
+      }
+
+      if (maxCount < wordCounts[word]) {
+        maxCount = wordCounts[word];
+        maxWord = word;
+      }
+    });
+
+  return maxWord;
+};
