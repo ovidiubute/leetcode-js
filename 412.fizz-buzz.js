6dd4ac181df1ee81ac3c537f988d95847ab06876
Ovidiu Bute <ovidiu.bute@gmail.com>
2018-11-03 17:39:17 +0200

    Add solution to 412.

---
+++ 412.fizz-buzz.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+  return Array.from({ length: n }, (i, v) => v + 1).map(i => {
+    if (i % 5 === 0) {
+      if (i % 3 === 0) {
+        return "FizzBuzz";
+      }
+
+      return "Buzz";
+    }
+
+    if (i % 3 === 0) {
+      return "Fizz";
+    }
+
+    return `${i}`;
+  });
+};
