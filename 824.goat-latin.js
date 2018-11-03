89c92a61882061c18c597cbb24be06425dea6be1
Ovidiu Bute <ovidiu.bute@gmail.com>
2018-11-03 19:05:40 +0200

    Add solution to 824.

---
+++ 824.goat-latin.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} S
+ * @return {string}
+ */
+var toGoatLatin = function(S) {
+  const vowels = ["a", "e", "i", "o", "u"];
+  const arr = S.split(" ");
+  for (let i = 0; i < arr.length; i++) {
+    if (vowels.includes(arr[i].toLowerCase().charAt(0))) {
+      arr[i] = arr[i] + "ma";
+    } else {
+      arr[i] = arr[i].substr(1) + arr[i].charAt(0) + "ma";
+    }
+
+    for (let j = 0; j < i + 1; j++) {
+      arr[i] = arr[i] + "a";
+    }
+  }
+
+  return arr.join(" ");
+};
