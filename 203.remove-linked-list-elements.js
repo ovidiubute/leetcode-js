4b6db45ce43edb58018a56d9d3aa24753a185b6a
Ovidiu Bute <ovidiu.bute@gmail.com>
2018-11-03 17:16:58 +0200

    Add solution to 203.

---
+++ 203.remove-linked-list-elements.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+  var helper = new ListNode(0);
+  helper.next = head;
+  var p = helper;
+
+  while (p.next) {
+    if (p.next.val === val) {
+      p.next = p.next.next;
+    } else {
+      p = p.next;
+    }
+  }
+
+  return helper.next;
+};
