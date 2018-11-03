98ef7d07694a99c700f4664839562fbe78c46a62
Ovidiu Bute <ovidiu.bute@gmail.com>
2018-11-03 18:28:24 +0200

    Add solution to 198.

---
+++ 198.house-robber.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+  if (nums.length === 0) {
+    return 0;
+  }
+  var even = 0;
+  var odd = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i % 2 === 0) {
+      even += nums[i];
+      even = even > odd ? even : odd;
+    } else {
+      odd += nums[i];
+      odd = even > odd ? even : odd;
+    }
+  }
+
+  return even > odd ? even : odd;
+};
