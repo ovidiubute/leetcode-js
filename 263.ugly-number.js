1733e5f13c3c67a274bdb29f47cd1fc0c8a6b2a2
Ovidiu Bute <ovidiu.bute@gmail.com>
2018-11-04 17:20:42 +0200

    Add solution to 263.

---
+++ 263.ugly-number.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isUgly = function(num) {
+  if (num === 0) {
+    return false;
+  }
+
+  if (num === 1) {
+    return true;
+  }
+
+  const divisors = [2, 3, 5];
+  for (d of divisors) {
+    while (num % d === 0) {
+      num = num / d;
+
+      if (num === 1) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+};
