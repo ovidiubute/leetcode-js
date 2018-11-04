c268d3f5091cc0f68b4440c10abdd4e75c81e2e1
Ovidiu Bute <ovidiu.bute@gmail.com>
2018-11-04 15:24:11 +0200

    Add solution to 896.

---
+++ 896.monotonic-array.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} A
+ * @return {boolean}
+ */
+var isMonotonic = function(A) {
+  if (A.length < 3) {
+    return true;
+  }
+
+  return isIncreasing(A) || isDecreasing(A);
+};
+
+var isIncreasing = function(A) {
+  for (let i = 1; i < A.length; i++) {
+    if (A[i - 1] > A[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+var isDecreasing = function(A) {
+  for (let i = 1; i < A.length; i++) {
+    if (A[i - 1] < A[i]) {
+      return false;
+    }
+  }
+  return true;
+};
