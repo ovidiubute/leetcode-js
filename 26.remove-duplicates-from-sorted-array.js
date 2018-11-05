e2a003288ac6b49ed37c5cac6b6f25a2a2c0137b
Ovidiu Bute <ovidiu.bute@gmail.com>
2018-11-05 08:17:36 +0200

    Add solution to 26.

---
+++ 26.remove-duplicates-from-sorted-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+  if (!nums.length) {
+    return 0;
+  }
+
+  var i = 0;
+  for (var j = 1; j < nums.length; j++) {
+    if (nums[i] != nums[j]) {
+      i++;
+      nums[i] = nums[j];
+    }
+  }
+
+  return i + 1;
+};
