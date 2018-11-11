5cbb2ee7d7f549c31e51489d902a54130c3cd56e
Ovidiu Bute <ovidiu.bute@gmail.com>
2018-11-11 21:16:23 +0200

    Add solution to 14.

---
+++ 0014.longest-common-prefix.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+  if (strs.length === 0) {
+    return "";
+  }
+
+  if (strs.length === 1) {
+    return strs[0];
+  }
+
+  prefix = strs[0];
+  for (let i = 1; i < strs.length; i++) {
+    prefix = commonPrefix(prefix, strs[i]);
+  }
+
+  return prefix;
+};
+
+var commonPrefix = function(prefix, str) {
+  let p = prefix;
+  while (p !== "") {
+    if (str.startsWith(p)) {
+      return p;
+    }
+
+    p = p.substr(0, p.length - 1);
+  }
+
+  return p;
+};
