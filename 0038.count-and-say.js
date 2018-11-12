05c2768ac8e09c69a321f194441a3c5dccdeb33f
Ovidiu Bute <ovidiu.bute@gmail.com>
2018-11-12 21:03:22 +0200

    Add solution to 38.

--- 0038.count-and-say.js
+++ 0038.count-and-say.js
@@ -6,4 +6,24 @@ var countAndSay = function(n) {
   if (n === 1) {
     return "1";
   }
+
+  const word = countAndSay(n - 1);
+  let count = 1;
+  let x = "";
+  let i = 0;
+
+  for (i = 0; i < word.length - 1; i++) {
+    if (word.charAt(i) === word.charAt(i + 1)) {
+      count++;
+    } else {
+      x += `${count}`;
+      x += word.charAt(i);
+      count = 1;
+    }
+  }
+
+  x += `${count}`;
+  x += word.charAt(i);
+
+  return x;
 };
