c54ae33b1356fdbd9ba1846219321539400905d3
Ovidiu Bute <ovidiu.bute@gmail.com>
2018-11-17 17:46:21 +0200

    Add solution to 88.

---
+++ 0088.merge-sorted-array.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+  if (nums1 === null || nums2 === null || n === 0) {
+    return;
+  }
+
+  let i = m - 1;
+  let j = n - 1;
+  let k = m + n - 1;
+
+  while (i >= 0 && j >= 0) {
+    if (nums1[i] >= nums2[j]) {
+      nums1[k--] = nums1[i--];
+    } else {
+      nums1[k--] = nums2[j--];
+    }
+  }
+
+  while (j >= 0) {
+    nums1[k--] = nums2[j--];
+  }
+};
