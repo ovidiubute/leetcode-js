348f90502262607e85e94b7ce46ff42df297a5ea
Ovidiu Bute <ovidiu.bute@gmail.com>
2018-12-28 18:13:22 +0200

    Add solution to 53.

---
+++ 0053.maximum-subarray.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+  if (nums.length === 1) {
+    return nums[0];
+  }
+
+  let s = nums[0];
+  let max = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    s = Math.max(nums[i], s + nums[i]);
+    max = Math.max(s, max);
+  }
+
+  return max;
+};
