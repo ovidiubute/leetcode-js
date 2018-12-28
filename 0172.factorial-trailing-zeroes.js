08e2591878bb999c16e09acd9bd1bc3ac07e30e5
Ovidiu Bute <ovidiu.bute@gmail.com>
2018-12-28 18:27:58 +0200

    Add solution to 172.

---
+++ 0172.factorial-trailing-zeroes.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var trailingZeroes = function(n) {
+  if (n === 0) {
+    return 0;
+  }
+
+  return Math.floor(n / 5) + trailingZeroes(Math.floor(n / 5));
+};
