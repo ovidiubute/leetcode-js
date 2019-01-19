3daa6635e7f8b89660cb855f9d5336f3bccb55a3
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-01-19 10:54:50 +0200

    Add solution to 171.

---
+++ 0171.excel-sheet-column-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var titleToNumber = function(s) {
+  let sum = 0;
+  for (let i = s.length - 1; i >= 0; i--) {
+    sum += Math.pow(26, i) * (s.charCodeAt(s.length - i - 1) - 64);
+  }
+
+  return sum;
+};
