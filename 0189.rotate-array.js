634d3d616ca62d160186ede824bacab13ba070fd
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-01-19 11:41:19 +0200

    Add solution to 189.

---
+++ 0189.rotate-array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+  var reverse = function(nums, start, end) {
+    while (start < end) {
+      let temp = nums[start];
+      nums[start] = nums[end];
+      nums[end] = temp;
+
+      start++;
+      end--;
+    }
+  };
+
+  k = k % nums.length;
+
+  reverse(nums, 0, nums.length - 1);
+  reverse(nums, 0, k - 1);
+  reverse(nums, k, nums.length - 1);
+};
