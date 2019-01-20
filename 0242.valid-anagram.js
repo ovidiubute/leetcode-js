8e0d88789f12aa79644b3a7cabe717b6a148a633
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-01-20 15:10:29 +0200

    Add solution to 242.

---
+++ 0242.valid-anagram.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+  let x = s.split("");
+  x.sort();
+
+  let y = t.split("");
+  y.sort();
+
+  return x.join("") === y.join("");
+};
