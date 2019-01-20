0866416b058858405afc8176afa795f3f2d85445
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-01-20 15:36:54 +0200

    Add solution to 283.

---
+++ 0283.move-zeroes.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+  let lastNonZeroIndex = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[lastNonZeroIndex++] = nums[i];
+    }
+  }
+
+  for (let i = lastNonZeroIndex; i < nums.length; i++) {
+    nums[i] = 0;
+  }
+};
