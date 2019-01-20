e7d001ce9bb3a990c4a853400726a2fe86b39725
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-01-20 16:10:49 +0200

    Add solution to 977.

---
+++ 0977.squares-of-a-sorted-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} A
+ * @return {number[]}
+ */
+var sortedSquares = function(A) {
+  A = A.map(x => (x *= x));
+  A.sort((a, b) => {
+    if (a < b) {
+      return -1;
+    }
+
+    if (a > b) {
+      return 1;
+    }
+
+    return 0;
+  });
+
+  return A;
+};
