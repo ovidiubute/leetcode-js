1a69faec944a909e188c0ee25d91f1fcb041e973
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-02 17:04:22 +0200

    Add solution for 551.

---
+++ 0551.student-attendance-record-I.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkRecord = function(s) {
+  let absences = 0;
+  let lastLate = Number.MIN_SAFE_INTEGER;
+  let prevLastLate = Number.MIN_SAFE_INTEGER;
+  for (let i = 0; i < s.length; i++) {
+    if (s.charAt(i) === "A") {
+      absences++;
+      if (absences > 1) {
+        return false;
+      }
+    }
+
+    if (s.charAt(i) === "L") {
+      if (lastLate === i - 1 && prevLastLate === i - 2) {
+        return false;
+      }
+      prevLastLate = lastLate;
+      lastLate = i;
+    }
+  }
+
+  return true;
+};
