1073f614661cc2bd82bb7c428415c7272be22969
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-02 10:31:44 +0200

    Add solution to 941.

---
+++ 0941.valid-mountain-array.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} A
+ * @return {boolean}
+ */
+var validMountainArray = function(A) {
+  let i = 0;
+
+  while (i + 1 < A.length && A[i] < A[i + 1]) {
+    i++;
+  }
+
+  if (i === 0 || i === A.length - 1) {
+    return false;
+  }
+
+  while (i + 1 < A.length && A[i] > A[i + 1]) {
+    i++;
+  }
+
+  return i === A.length - 1;
+};
