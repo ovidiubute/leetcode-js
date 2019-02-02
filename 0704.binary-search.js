262b6e5a2a6bf4e6802bfda2859ae6a59b66e5cc
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-02 21:20:36 +0200

    Add solution for 704.

---
+++ 0704.binary-search.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+  let start = 0;
+  let end = nums.length - 1;
+
+  while (end >= start) {
+    let mid = Math.floor((start + end) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] > target) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+
+  return -1;
+};
