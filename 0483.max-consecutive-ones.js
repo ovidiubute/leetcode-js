6c084c84e35706e32d18225e6a17e6dcf4b94b64
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-02 17:19:40 +0200

    Add solution for 483.

---
+++ 0483.max-consecutive-ones.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+  let max = 0;
+  let localMax = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 1) {
+      localMax++;
+    } else {
+      localMax = 0;
+    }
+
+    max = Math.max(max, localMax);
+  }
+
+  return max;
+};
