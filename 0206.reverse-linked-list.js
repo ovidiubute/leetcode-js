8a1c476e495380c110a1e5bc98761edab4fa436b
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-02 16:15:54 +0200

    Add solution for 206.

---
+++ 0206.reverse-linked-list.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+  const stack = [];
+
+  if (head === null) {
+    return head;
+  }
+
+  let p = head;
+
+  while (p) {
+    stack.push(p);
+    p = p.next;
+  }
+
+  let item = stack.pop();
+  let h = item;
+  while (stack.length) {
+    const nextItem = stack.pop();
+    item.next = nextItem;
+    item = item.next;
+  }
+
+  item.next = null;
+
+  return h;
+};
