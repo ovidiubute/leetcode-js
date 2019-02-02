f1fdf9771c6774b31980c56593c5088ca745a29a
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-02 18:43:37 +0200

    Add solution for 367.

---
+++ 0367.valid-perfect-square.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+  let start = 0;
+  let end = num;
+  let mid = null;
+
+  while (end >= start) {
+    let mid = Math.floor((end + start) / 2);
+    if (mid * mid > num) {
+      end = mid - 1;
+    } else if (mid * mid < num) {
+      start = mid + 1;
+    } else {
+      return true;
+    }
+  }
+
+  return false;
+};
