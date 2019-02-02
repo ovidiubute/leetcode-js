42b1da0ebab261482ec607614b76043106729a0b
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-02 21:26:46 +0200

    Add solution to 709.

---
+++ 0709.to-lower-case.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} str
+ * @return {string}
+ */
+var toLowerCase = function(str) {
+  let result = "";
+  for (let c of str) {
+    let code = c.charCodeAt(0);
+    if (code > 64 && code < 97) {
+      result += String.fromCharCode(code + 32);
+    } else {
+      result += c;
+    }
+  }
+
+  return result;
+};
