dbb94894f64cb2fc4ce090134ac6ce2612f79f0f
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-02 17:37:41 +0200

    Add solution to 605.

---
+++ 0605.can-place-flowers.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+  let i = 0;
+  let count = 0;
+
+  while (i < flowerbed.length) {
+    if (
+      flowerbed[i] === 0 &&
+      (i === 0 || flowerbed[i - 1] === 0) &&
+      (i == flowerbed.length - 1 || flowerbed[i + 1] === 0)
+    ) {
+      flowerbed[i] = 1;
+      count++;
+    }
+
+    i++;
+  }
+
+  return count >= n;
+};
