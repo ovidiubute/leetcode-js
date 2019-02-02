e08abdb550dc2f526fe92602209dd79efd82e605
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-02 11:19:17 +0200

    Add solution to 599.

---
+++ 0599.minimum-index-sum-of-two-lists.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string[]} list1
+ * @param {string[]} list2
+ * @return {string[]}
+ */
+var findRestaurant = function(list1, list2) {
+  const common = {};
+  for (let i = 0; i < list1.length; i++) {
+    const j = list2.indexOf(list1[i]);
+    if (j !== -1) {
+      common[list1[i]] = i + j;
+    }
+  }
+
+  const output = [];
+  const min = Math.min(...Object.keys(common).map(k => common[k]));
+
+  Object.keys(common).map(k => {
+    if (common[k] === min) {
+      output.push(k);
+    }
+  });
+
+  return output;
+};
