e670b9ed938301ef2af793a1176a22e190918df8
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-02 18:05:31 +0200

    Add solution for 162.

---
+++ 0162.find-peak-element.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+  if (nums.length === 1) {
+    return 0;
+  }
+
+  let prev = Number.MIN_SAFE_INTEGER;
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] > prev && nums[i] > nums[i + 1]) {
+      return i;
+    }
+
+    prev = nums[i];
+  }
+
+  if (prev !== Number.MIN_SAFE_INTEGER) {
+    if (nums[nums.length - 1] > nums[nums.length - 2]) {
+      return nums.length - 1;
+    }
+  }
+};
