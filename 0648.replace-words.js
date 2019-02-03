79a2afa228daf540f66c3bd1612ddf6ba13429a1
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-03 11:45:32 +0200

    Add solution to 648.

---
+++ 0648.replace-words.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string[]} dict
+ * @param {string} sentence
+ * @return {string}
+ */
+var replaceWords = function(dict, sentence) {
+  const rootset = new Set(dict);
+
+  let result = "";
+  const words = sentence.split(" ");
+  for (w of words) {
+    let prefix = "";
+    for (let i = 0; i < w.length; i++) {
+      prefix = w.slice(0, i + 1);
+      if (rootset.has(prefix)) {
+        break;
+      }
+    }
+
+    if (result.length > 0) {
+      result += " ";
+    }
+    result += prefix;
+  }
+
+  return result;
+};
