b8d08d8f13dc38f4bb6e4fbc35269048410baafd
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-03 15:32:56 +0200

    Move solutions under new directory.

--- update-readme.js
+++ update-readme.js
@@ -1,6 +1,6 @@
 const problemCount = require("./.leetcode").total;
 
-const script = "ls | grep -E [0-9]{4}.[a-zA-Z1-9-]*.js | wc -l";
+const script = "ls solutions | grep -E [0-9]{4}.[a-zA-Z1-9-]*.js | wc -l";
 const solutionCount = require("child_process").execSync(script);
 
 const progress = Number(((solutionCount / problemCount) * 100).toFixed(2));
