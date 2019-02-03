918a62f3dff181747f5629293fc7c7d9d35512ee
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-03 10:05:19 +0200

    Add solution to 187.

---
+++ 0187.repeated-dna-sequences.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {string[]}
+ */
+var findRepeatedDnaSequences = function(s) {
+  const frequency = {};
+  for (let i = 0; i < s.length; i++) {
+    const sequence = s.slice(i, i + 10);
+    if (frequency[sequence] === undefined) {
+      frequency[sequence] = 1;
+    } else {
+      frequency[sequence]++;
+    }
+  }
+
+  return Object.keys(frequency).filter(sequence => frequency[sequence] > 1);
+};
