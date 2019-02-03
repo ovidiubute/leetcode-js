e3fc5f7d6353fed1110442ddfcac1ead5e0f94e0
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-03 16:22:15 +0200

    Fix badge generation script.

--- update-badge.js
+++ update-badge.js
@@ -1,19 +1,33 @@
-const problemCount = require("./.leetcode").total;
+// Update this over time from leetcode.com
+const TOTAL_PROBLEM_COUNT = 932;
 
-const script = "ls solutions | grep -E [0-9]{4}.[a-zA-Z1-9-]*.js | wc -l";
-const solutionCount = require("child_process").execSync(script);
+// Count how many solutions we have so far
+const countSolutionsScript =
+  "ls solutions | grep -E [0-9]{4}.[a-zA-Z1-9-]*.js | wc -l";
+const solutionCount = require("child_process").execSync(countSolutionsScript);
 
-const progress = Number(((solutionCount / problemCount) * 100).toFixed(2));
+// Compute our progress
+const progress = Number(
+  ((solutionCount / TOTAL_PROBLEM_COUNT) * 100).toFixed(2)
+);
+
+// Badge data for shields.io
 const color = progress > 15 ? "yellow" : progress > 30 ? "green" : "red";
+const badge = {
+  schemaVersion: 1,
+  label: "progress",
+  message: `${progress}%`,
+  color,
+  namedLogo: "leetcode"
+};
 
-const badge = `
-{
-  "schemaVersion": 1,
-  "label": "progress",
-  "message": "${progress}%",
-  "color": "${color}",
-  "namedLogo": "leetcode"
-}
-`;
+// Run output through prettier
+const prettier = require("prettier");
+const output = prettier.format(JSON.stringify(badge), {
+  parser: "json"
+});
 
-require("child_process").execSync(`echo "${badge}" > .leetcode.json`);
+// Write shields.io badge data to disk
+require("fs").writeFileSync("./.leetcode.json", output, {
+  flag: "w"
+});
