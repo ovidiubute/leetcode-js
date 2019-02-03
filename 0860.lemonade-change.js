218e81504ac0ce3a678bdb85d1dc56e6cbe12825
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-03 10:45:46 +0200

    Add solution to 860.

---
+++ 0860.lemonade-change.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} bills
+ * @return {boolean}
+ */
+var lemonadeChange = function(bills) {
+  let fives = 0,
+    tens = 0;
+  for (let i = 0; i < bills.length; i++) {
+    if (bills[i] === 5) {
+      fives++;
+    } else if (bills[i] === 10) {
+      if (!fives) {
+        return false;
+      } else {
+        fives--;
+        tens++;
+      }
+    } else {
+      // Check this branch first because for large inputs
+      // you will run out of small bills faster.
+      if (fives > 0 && tens > 0) {
+        fives--;
+        tens--;
+      } else if (fives > 2) {
+        fives -= 3;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return true;
+};
