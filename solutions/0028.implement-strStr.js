56af493d34f79b3d7009aa70284a503d5092b682
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-04 08:38:15 +0200

    Add solution for 28.

---
+++ solutions/0028.implement-strStr.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+  if (!needle.length) {
+    return 0;
+  }
+
+  if (!haystack.length) {
+    return -1;
+  }
+
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack.charAt(i) === needle.charAt(0)) {
+      if (haystack.slice(i, i + needle.length) === needle) {
+        return i;
+      }
+    }
+  }
+
+  return -1;
+};
