efe52ff3b6dd97616fba2dc6499d092129cfb876
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-04 07:58:01 +0200

    Add solution for 686.

---
+++ solutions/0686.repeated-string-match.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} A
+ * @param {string} B
+ * @return {number}
+ */
+var repeatedStringMatch = function(A, B) {
+  let i = 0;
+  let S = "";
+  while (S.length < 10000 && S.indexOf(B) === -1) {
+    S += A;
+    i++;
+  }
+
+  if (S.indexOf(B) !== -1) {
+    return i;
+  }
+
+  return -1;
+};
