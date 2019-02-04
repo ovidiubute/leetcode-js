f64faf7017e15ca7dc948ef9582903e8bbb7e2fc
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-04 22:36:37 +0200

    Add solution for 788.

---
+++ solutions/0788.rotated-digits.js
@@ -0,0 +1,35 @@
+var isValid = function(x) {
+  let ok = false;
+  do {
+    let c = x % 10;
+
+    if (c === 3 || c === 4 || c === 7) {
+      return false;
+    }
+
+    if (!ok) {
+      if (c === 2 || c === 5 || c === 6 || c === 9) {
+        ok = true;
+      }
+    }
+
+    x = Math.floor(x / 10);
+  } while (x > 0);
+
+  return ok;
+};
+
+/**
+ * @param {number} N
+ * @return {number}
+ */
+var rotatedDigits = function(N) {
+  let count = 0;
+  for (let i = 2; i <= N; i++) {
+    if (isValid(i)) {
+      count++;
+    }
+  }
+
+  return count;
+};
