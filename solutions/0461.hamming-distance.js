f2cbed834fe1797a206b17ca8471666adcde03f4
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-04 08:28:33 +0200

    Add solution for 461.

---
+++ solutions/0461.hamming-distance.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+var hammingDistance = function(x, y) {
+  var hammingWeight = function(n) {
+    let mask = 1;
+    let count = 0;
+    while (mask > 0) {
+      if ((n & mask) !== 0) {
+        count++;
+      }
+
+      mask = mask << 1;
+    }
+
+    return count;
+  };
+
+  return hammingWeight(x ^ y);
+};
