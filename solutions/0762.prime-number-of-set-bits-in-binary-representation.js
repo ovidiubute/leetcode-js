08a110d08949603b4f88b969a04d128f93cedc6b
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-09 11:29:41 +0200

    Add solution to 762.

---
+++ solutions/0762.prime-number-of-set-bits-in-binary-representation.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} L
+ * @param {number} R
+ * @return {number}
+ */
+var countPrimeSetBits = function(L, R) {
+  let count = 0;
+  const primes = [2, 3, 5, 7, 11, 13, 17, 19];
+
+  for (let i = L; i <= R; i++) {
+    const str = Number(i).toString(2);
+    const chars = [];
+    for (let j = 0; j < str.length; j++) {
+      chars.push(str.charAt(j));
+    }
+
+    let ones = 0;
+    for (let k = 0; k < chars.length; k++) {
+      if (chars[k] === "1") ones++;
+    }
+
+    if (primes.includes(ones)) count++;
+  }
+
+  return count;
+};
