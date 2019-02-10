ee98133e3c645b4fa4c8dbe62685b669442a147d
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-10 16:33:25 +0200

    Add solution to 771.

---
+++ solutions/0771.jewels-and-stones.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} J
+ * @param {string} S
+ * @return {number}
+ */
+var numJewelsInStones = function(J, S) {
+  const jewels = new Set(J.split(""));
+  let count = 0;
+
+  for (let i = 0; i < S.length; i++) {
+    if (jewels.has(S.charAt(i))) {
+      count++;
+    }
+  }
+
+  return count;
+};
