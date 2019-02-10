e758ca549a710ef13c32a0c442b2bd1a6448c2d6
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-11 00:02:13 +0200

    Add solution for 66.

---
+++ solutions/0066.plus-one.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+  let carry = 1;
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (carry) digits[i] += carry;
+
+    if (digits[i] > 9) {
+      digits[i] %= 10;
+      carry = 1;
+    } else {
+      carry = 0;
+    }
+  }
+
+  if (carry) return [1].concat(digits);
+
+  return digits;
+};
