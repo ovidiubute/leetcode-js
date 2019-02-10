cac750db2818255e1605d802fc8d483d346c7f93
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-10 17:37:22 +0200

    Add solution to 717.

---
+++ solutions/0717.1-bit-and-2-bit-characters.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} bits
+ * @return {boolean}
+ */
+var isOneBitCharacter = function(bits) {
+  let i = 0;
+  while (i < bits.length - 1) {
+    i += bits[i] + 1;
+  }
+
+  return i === bits.length - 1;
+};
