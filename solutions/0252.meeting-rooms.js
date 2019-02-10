07e17791bba8a66fce02bf6724741a383b15a7f0
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-10 23:34:12 +0200

    Add solution for 252.

---
+++ solutions/0252.meeting-rooms.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for an interval.
+ * function Interval(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ * }
+ */
+/**
+ * @param {Interval[]} intervals
+ * @return {boolean}
+ */
+var canAttendMeetings = function(intervals) {
+  intervals.sort((a, b) => {
+    return a.start - b.start;
+  });
+
+  for (let i = 0; i < intervals.length - 1; i++) {
+    if (intervals[i].end > intervals[i + 1].start) {
+      return false;
+    }
+  }
+
+  return true;
+};
