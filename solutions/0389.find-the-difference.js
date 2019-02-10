17974ebbe58bbb62de8e5f2ff58829a6aef64713
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-10 23:15:41 +0200

    Add solution for 389.

---
+++ solutions/0389.find-the-difference.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function(s, t) {
+  let sumS = 0;
+  let sumT = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    sumS += s.charCodeAt(i);
+    sumT += t.charCodeAt(i);
+  }
+
+  sumT += t.charCodeAt(t.length - 1);
+
+  return String.fromCharCode(sumT - sumS);
+};
