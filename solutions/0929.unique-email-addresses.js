83196c10021121539de2e8898236a131697a09be
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-10 17:50:25 +0200

    Add solution for 929.

---
+++ solutions/0929.unique-email-addresses.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} emails
+ * @return {number}
+ */
+var numUniqueEmails = function(emails) {
+  const uniqueEmails = new Set();
+
+  for (let i = 0; i < emails.length; i++) {
+    const [local, domain] = emails[i].split("@");
+
+    const noDots = local.replace(/[.]*/gi, "");
+    const noFilter = noDots.split("+")[0];
+
+    uniqueEmails.add(noFilter + "@" + domain);
+  }
+
+  return uniqueEmails.size;
+};
