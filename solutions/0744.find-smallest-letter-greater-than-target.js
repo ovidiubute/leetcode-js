a14e78db33c49bcd9152d6f971b5680fbad03c68
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-10 16:52:41 +0200

    Add solution to 744.

---
+++ solutions/0744.find-smallest-letter-greater-than-target.js
@@ -0,0 +1,14 @@
+/**
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+var nextGreatestLetter = function(letters, target) {
+  for (let i = 0; i < letters.length; i++) {
+    if (target < letters[i]) {
+      return letters[i];
+    }
+  }
+
+  return letters[0];
+};
