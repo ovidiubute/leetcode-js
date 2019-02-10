c470aede8c17b0c05bdac6b3a6c1a66bd825b1ad
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-10 20:20:58 +0200

    Add solution for 359.

---
+++ solutions/0359.logger-rate-limiter.js
@@ -0,0 +1,36 @@
+/**
+ * Initialize your data structure here.
+ */
+var Logger = function() {
+  this.history = {};
+};
+
+/**
+ * Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. 
+ * @param {number} timestamp 
+ * @param {string} message
+ * @return {boolean}
+ */
+Logger.prototype.shouldPrintMessage = function(timestamp, message) {
+  if (this.history[message] === undefined) {
+    this.history[message] = timestamp;
+
+    return true;
+  }
+
+  if (timestamp - this.history[message] >= 10) {
+    this.history[message] = timestamp;
+
+    return true;
+  }
+
+  return false;
+};
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * var obj = Object.create(Logger).createNew()
+ * var param_1 = obj.shouldPrintMessage(timestamp,message)
+ */
