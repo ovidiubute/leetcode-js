0e2e90c49f6716e662943b1f0425b599e9bce652
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-10 17:59:36 +0200

    Add solution to 657.

---
+++ solutions/0657.robot-return-to-origin.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+var judgeCircle = function(moves) {
+  let x = 0;
+  let y = 0;
+
+  for (let i = 0; i < moves.length; i++) {
+    switch (moves[i]) {
+      case "L":
+        y--;
+        break;
+      case "R":
+        y++;
+        break;
+      case "U":
+        x++;
+        break;
+      default:
+        x--;
+        break;
+    }
+  }
+
+  return x === 0 && y === 0;
+};
