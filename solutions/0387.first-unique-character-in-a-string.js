8e90d3baf4552f001df475ede2325109c148c0bd
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-10 22:26:28 +0200

    Add solution for 387.

---
+++ solutions/0387.first-unique-character-in-a-string.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+  const freq = {};
+
+  for (let i = 0; i < s.length; i++) {
+    if (freq[s.charAt(i)] === undefined) {
+      freq[s.charAt(i)] = 1;
+    } else {
+      freq[s.charAt(i)]++;
+    }
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    if (freq[s.charAt(i)] === 1) {
+      return i;
+    }
+  }
+
+  return -1;
+};
