cbbbd728b74a7b81b14fcd8de51b112601a33f7a
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-10 17:10:51 +0200

    Add solution to 984.

---
+++ solutions/0984.string-without-AAA-or-BBB.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} A
+ * @param {number} B
+ * @return {string}
+ */
+var strWithout3a3b = function(A, B) {
+  let result = [];
+
+  while (A || B) {
+    let writeA;
+
+    if (
+      result.length >= 2 &&
+      result[result.length - 1] === result[result.length - 2]
+    ) {
+      writeA = result[result.length - 1] === "b";
+    } else {
+      writeA = A >= B;
+    }
+
+    if (writeA) {
+      A--;
+      result.push("a");
+    } else {
+      B--;
+      result.push("b");
+    }
+  }
+
+  return result.join("");
+};
