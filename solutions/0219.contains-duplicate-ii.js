15228a12081895cb666789ad7a8a499675999a47
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-10 22:46:30 +0200

    Add solution for 219.

---
+++ solutions/0219.contains-duplicate-ii.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+  const seen = new Map();
+
+  for (let i = 0; i < nums.length; ++i) {
+    if (seen.has(nums[i])) {
+      return true;
+    }
+
+    seen.set(nums[i], 1);
+    if (seen.size > k) {
+      seen.delete(nums[i - k]);
+    }
+  }
+
+  return false;
+};
