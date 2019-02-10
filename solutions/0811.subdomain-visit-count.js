92acc004bdd4f6aa4d96dcbbd5d18ef0a649c49e
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-10 22:15:27 +0200

    Add solution to 811.

---
+++ solutions/0811.subdomain-visit-count.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string[]} cpdomains
+ * @return {string[]}
+ */
+var subdomainVisits = function(cpdomains) {
+  const domainVisits = {};
+
+  for (const cpdomain of Object.values(cpdomains)) {
+    let [hits, domain] = cpdomain.split(" ");
+    const subdomains = domain.split(".");
+
+    let d = "";
+    for (let i = subdomains.length - 1; i >= 0; --i) {
+      d = subdomains[i] + (i < subdomains.length - 1 ? "." : "") + d;
+
+      if (domainVisits[d] === undefined) {
+        domainVisits[d] = Number.parseInt(hits);
+      } else {
+        domainVisits[d] += Number.parseInt(hits);
+      }
+    }
+  }
+
+  return Object.keys(domainVisits).map(d => `${domainVisits[d]} ${d}`);
+};
