3d4267a2d6da9d47d065ef4877bc7a684bd0b346
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-10 20:10:04 +0200

    Add solutions.

---
+++ solutions/0561.array-partition-i.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayPairSum = function(nums) {
+  nums.sort((a, b) => {
+    return a > b ? 1 : a < b ? -1 : 0;
+  });
+
+  let sum = 0;
+  for (let i = 0; i < nums.length; i += 2) {
+    sum += nums[i];
+  }
+
+  return sum;
+};
---
+++ solutions/0942.di-string-match.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} S
+ * @return {number[]}
+ */
+var diStringMatch = function(S) {
+  const result = [];
+  let lo = 0,
+    hi = S.length;
+
+  for (let i = 0; i < S.length; i++) {
+    if (S.charAt(i) === "I") {
+      result.push(lo);
+      lo++;
+    } else {
+      result.push(hi);
+      hi--;
+    }
+  }
+
+  return result.concat([lo]);
+};
