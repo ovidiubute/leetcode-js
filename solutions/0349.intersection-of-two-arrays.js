2ee3470780e136b6d329b22bf2f404141231f05a
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-17 17:21:11 +0200

    Add solution for 349.

---
+++ solutions/0349.intersection-of-two-arrays.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+  const nums = nums1.length > nums2.length ? nums2 : nums1;
+  const uset = new Set(nums);
+
+  const out = [];
+  for (num of uset) {
+    if (nums1.length > nums2.length) {
+      if (nums1.indexOf(num) !== -1) out.push(num);
+    } else {
+      if (nums2.indexOf(num) !== -1) out.push(num);
+    }
+  }
+
+  return out;
+};
