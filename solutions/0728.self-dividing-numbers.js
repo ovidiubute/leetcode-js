21123df0042d0d9ba421325895ed3be01fdf1601
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-17 15:13:27 +0200

    Add solution for 728.

---
+++ solutions/0728.self-dividing-numbers.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number[]}
+ */
+var selfDividingNumbers = function(left, right) {
+  const results = [];
+
+  for (let x = left; x <= right; x++) {
+    let y = x;
+    let ok = true;
+    while (y && ok) {
+      let c = y % 10;
+
+      if (c === 0 || x % c > 0) ok = false;
+
+      y = Math.floor(y / 10);
+    }
+
+    if (ok) results.push(x);
+  }
+
+  return results;
+};
