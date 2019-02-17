5fa5f4f2e9a3b34363212c1ee64db261498bd90d
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-17 11:18:18 +0200

    Add solutions.

---
+++ solutions/0346.moving-average-from-data-stream.js
@@ -0,0 +1,33 @@
+/**
+ * Initialize your data structure here.
+ * @param {number} size
+ */
+var MovingAverage = function(size) {
+  this.values = [];
+  this.size = size;
+};
+
+/**
+ * @param {number} val
+ * @return {number}
+ */
+MovingAverage.prototype.next = function(val) {
+  this.values.push(val);
+
+  if (this.values.length > this.size) {
+    this.values.splice(0, 1);
+  }
+
+  let sum = 0;
+  for (let i = 0; i < this.values.length; i++) {
+    sum += this.values[i];
+  }
+
+  return sum / this.values.length;
+};
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * var obj = Object.create(MovingAverage).createNew(size)
+ * var param_1 = obj.next(val)
+ */
---
+++ solutions/0350.intersection-of-two-arrays-ii.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+  const nums1Seen = {};
+  const nums2Seen = {};
+
+  for (let i = 0; i < nums1.length; i++) {
+    if (nums1Seen[nums1[i]] === undefined) {
+      nums1Seen[nums1[i]] = 1;
+    } else {
+      nums1Seen[nums1[i]]++;
+    }
+  }
+
+  for (let i = 0; i < nums2.length; i++) {
+    if (nums2Seen[nums2[i]] === undefined) {
+      nums2Seen[nums2[i]] = 1;
+    } else {
+      nums2Seen[nums2[i]]++;
+    }
+  }
+
+  const result = [];
+  for ([number, frequency] of Object.entries(nums1Seen)) {
+    if (!nums2Seen[number]) continue;
+
+    let times = Math.min(frequency, nums2Seen[number]);
+    while (times) {
+      result.push(number);
+      times--;
+    }
+  }
+
+  return result;
+};
