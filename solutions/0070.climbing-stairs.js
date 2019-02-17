1870e4fc0348927c0a73ab1d0522e4efe390382f
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-17 09:25:31 +0200

    Add solution to 70.

---
+++ solutions/0070.climbing-stairs.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+  let memo = [];
+  return steps(0, n, memo);
+};
+
+var steps = function(i, n, memo) {
+  if (i > n) return 0;
+
+  if (i === n) return 1;
+
+  if (memo[i] > 0) return memo[i];
+
+  memo[i] = steps(i + 1, n, memo) + steps(i + 2, n, memo);
+  return memo[i];
+};
