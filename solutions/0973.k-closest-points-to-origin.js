20b2f3f904948dbbee6722e013aeb87dd3aa7fcf
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-17 16:34:54 +0200

    Add solution for 973.

---
+++ solutions/0973.k-closest-points-to-origin.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[][]} points
+ * @param {number} K
+ * @return {number[][]}
+ */
+var kClosest = function(points, K) {
+  points.sort((a, b) => {
+    const da = dist(a);
+    const db = dist(b);
+    if (da > db) {
+      return 1;
+    } else if (da < db) {
+      return -1;
+    }
+
+    return 0;
+  });
+
+  const out = [];
+  for (let i = 0; i < K; i++) {
+    out.push(points[i]);
+  }
+  return out;
+};
+
+var dist = function(a) {
+  return a[0] ** 2 + a[1] ** 2;
+};
