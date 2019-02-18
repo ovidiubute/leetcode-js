24af482a02439dac95faf95390b67c8fa8f250d9
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-18 22:40:03 +0200

    Add solution to 448.

---
+++ solutions/0448.find-all-numbers-disappeared-in-an-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+  const seen = {};
+  for (let i = 1; i <= nums.length; i++) {
+    seen[i] = 0;
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    seen[nums[i]]++;
+  }
+
+  return Object.keys(seen).filter(k => {
+    return seen[k] === 0;
+  });
+};
