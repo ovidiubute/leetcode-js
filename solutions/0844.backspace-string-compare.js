86b3be5629e01428f00c1715996d4fb29a724f00
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-18 21:13:05 +0200

    Add solution to 844.

---
+++ solutions/0844.backspace-string-compare.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} S
+ * @param {string} T
+ * @return {boolean}
+ */
+var backspaceCompare = function(S, T) {
+  return typed(S) === typed(T);
+};
+
+var typed = function(S) {
+  const stack = [];
+  for (let i = 0; i < S.length; i++) {
+    if (S.charAt(i) === "#") stack.pop();
+    else stack.push(S.charAt(i));
+  }
+
+  return stack.join("");
+};
