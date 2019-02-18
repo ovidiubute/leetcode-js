afaf047e845635c9df4293d9b2c20e6e71cfad7c
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-18 20:51:24 +0200

    Add solution to 482.

---
+++ solutions/0482.license-key-formatting.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} S
+ * @param {number} K
+ * @return {string}
+ */
+var licenseKeyFormatting = function(S, K) {
+  const out = [];
+
+  for (let i = S.length - 1, rem = K + 1; i >= 0; i--) {
+    if (S.charAt(i) === "-") continue;
+
+    if (--rem === 0) {
+      out.push("-");
+      rem = K;
+    }
+
+    out.push(S.charAt(i).toUpperCase());
+  }
+
+  return out.reverse().join("");
+};
