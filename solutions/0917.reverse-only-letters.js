65717b9ed2aa4644312db1e7d340c3b53dab1ce2
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-22 00:08:26 +0200

    Add solution to 917.

---
+++ solutions/0917.reverse-only-letters.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} S
+ * @return {string}
+ */
+var reverseOnlyLetters = function(S) {
+  const stack = [];
+  const out = new Array(S.length).map(x => undefined);
+
+  for (let i = 0; i < S.length; i++) {
+    const c = S.charAt(i);
+    if (isLetter(c)) {
+      stack.push(c);
+    } else {
+      out[i] = c;
+    }
+  }
+
+  for (let i = 0; i < out.length; i++) {
+    if (out[i] === undefined) {
+      out[i] = stack.pop();
+    }
+  }
+
+  return out.join("");
+};
+
+var isLetter = function(a) {
+  const c = a.charCodeAt(0);
+
+  return (c >= 65 && c <= 90) || (c >= 97 && c <= 122);
+};
