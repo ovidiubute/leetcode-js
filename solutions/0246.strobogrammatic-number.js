6cd10829edc4fb79702ef2aab8ab12a7f5b32dc6
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-22 23:35:05 +0200

    Add solution to 246.

---
+++ solutions/0246.strobogrammatic-number.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} num
+ * @return {boolean}
+ */
+var isStrobogrammatic = function(num) {
+  const strob = {
+    0: 0,
+    1: 1,
+    2: -1,
+    3: -1,
+    4: -1,
+    5: -1,
+    6: 9,
+    7: -1,
+    8: 8,
+    9: 6
+  };
+
+  let i = 0,
+    j = num.length - 1;
+  while (i <= j) {
+    const char1 = Number.parseInt(num.charAt(i));
+    const char2 = Number.parseInt(num.charAt(j));
+
+    if (strob[char1] === -1 || strob[char2] === -1) {
+      return false;
+    }
+
+    if (strob[char1] !== char2) {
+      return false;
+    }
+
+    i++;
+    j--;
+  }
+
+  return true;
+};
