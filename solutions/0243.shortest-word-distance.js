de924ba20933d14177309ec4bc82ce8f45aacdfd
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-22 23:17:57 +0200

    Add solution for 243.

---
+++ solutions/0243.shortest-word-distance.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string[]} words
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var shortestDistance = function(words, word1, word2) {
+  const positions = {
+    [word1]: [],
+    [word2]: []
+  };
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i] === word1) positions[word1].push(i);
+    else if (words[i] === word2) positions[word2].push(i);
+  }
+
+  let min = Number.MAX_SAFE_INTEGER;
+  for (let i = 0; i < positions[word1].length; i++) {
+    for (let j = 0; j < positions[word2].length; j++) {
+      const diff = Math.abs(positions[word1][i] - positions[word2][j]);
+      if (diff < min) min = diff;
+    }
+  }
+
+  return min;
+};
