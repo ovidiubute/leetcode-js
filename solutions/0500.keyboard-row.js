1b1081ad7d1f4e46852a76d6d48c1304872c3203
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-23 09:18:25 +0200

    Add solution for 500.

---
+++ solutions/0500.keyboard-row.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var findWords = function(words) {
+  const rows = [
+    ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
+    ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
+    ["z", "x", "c", "v", "b", "n", "m"]
+  ];
+  const out = [];
+
+  for (let i = 0; i < words.length; i++) {
+    let f = false,
+      s = false,
+      t = false,
+      ok = true;
+    for (let j = 0; j < words[i].length; j++) {
+      if (rows[0].includes(words[i].charAt(j))) f = true;
+      if (rows[1].includes(words[i].charAt(j))) s = true;
+      if (rows[2].includes(words[i].charAt(j))) t = true;
+
+      if ((f && s) || (f && t) || (s && t)) {
+        ok = false;
+        break;
+      }
+    }
+
+    ok && out.push(words[i]);
+  }
+
+  return out;
+};
