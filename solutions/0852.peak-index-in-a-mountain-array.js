4bbd5bba342844f3c7b08c66e7d91e49c7a69c54
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-24 14:42:09 +0200

    Add solution to 852.

---
+++ solutions/0852.peak-index-in-a-mountain-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} A
+ * @return {number}
+ */
+var peakIndexInMountainArray = function(A) {
+  let x = A[0];
+  for (let i = 1; i < A.length; i++) {
+    if (A[i] < x) {
+      return i - 1;
+    }
+
+    x = A[i];
+  }
+};
