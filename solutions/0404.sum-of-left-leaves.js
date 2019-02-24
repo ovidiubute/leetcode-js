2958d6128a19dcf3e2516c8b55f49fa311f322b9
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-24 17:20:05 +0200

    Add solution to 404.

---
+++ solutions/0404.sum-of-left-leaves.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumOfLeftLeaves = function(root) {
+  if (root === null) {
+    return 0;
+  }
+
+  this.s = { total: 0 };
+
+  sum(root, this.s);
+
+  return s.total;
+};
+
+var sum = function(node, s) {
+  if (node === null) {
+    return 0;
+  }
+
+  if (node.left && isLeaf(node.left)) {
+    s.total += node.left.val;
+  }
+
+  sum(node.left, s);
+  sum(node.right, s);
+};
+
+var isLeaf = function(node) {
+  return node !== null && node.left === null && node.right === null;
+};
