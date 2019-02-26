5554ae6e6e6ec217a4c542559180a5520e1a0957
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-26 23:30:37 +0200

    Add solution for 633.

---
+++ solutions/0633.sum-of-square-numbers.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} c
+ * @return {boolean}
+ */
+var judgeSquareSum = function(c) {
+  for (let a = 0; a ** 2 <= c; a++) {
+    if (Number.isInteger(Math.sqrt(c - a ** 2))) {
+      return true;
+    }
+  }
+
+  return false;
+};
