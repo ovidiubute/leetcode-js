7bf021b411eb0b209437a712fdfe7769918793f2
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-26 23:42:59 +0200

    Add solution to 520.

---
+++ solutions/0520.detect-capital.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var detectCapitalUse = function(word) {
+  if (word.toUpperCase() === word) {
+    return true;
+  }
+
+  if (word.toLowerCase() === word) {
+    return true;
+  }
+
+  if (word.length > 1 && word.charAt(0) === word.charAt(0).toUpperCase()) {
+    const substr = word.slice(1);
+    if (substr.toLowerCase() === substr) {
+      return true;
+    }
+  }
+
+  return false;
+};
