a12ccc672a78cd7996ac01ec12d22d56a3eb53ef
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-26 22:40:01 +0200

    Add solution to 628.

---
+++ solutions/0628.maximum-product-of-three-numbers.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumProduct = function(nums) {
+  nums.sort((a, b) => {
+    return a - b;
+  });
+
+  const len = nums.length;
+
+  return Math.max(
+    nums[len - 1] * nums[len - 2] * nums[len - 3],
+    nums[0] * nums[1] * nums[len - 1]
+  );
+};
