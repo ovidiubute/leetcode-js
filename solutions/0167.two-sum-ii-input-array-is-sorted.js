948c10725329958c1cbd95ac2df90608df387d86
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-28 23:22:11 +0200

    Add solution for 167.

---
+++ solutions/0167.two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+  const positions = {};
+  for (let i = 0; i < numbers.length; i++) {
+    if (positions[numbers[i]] === undefined) {
+      positions[numbers[i]] = [i + 1];
+    } else {
+      positions[numbers[i]].push(i + 1);
+    }
+  }
+
+  for (let i = 0; i < numbers.length; i++) {
+    const rest = positions[target - numbers[i]];
+    if (rest !== undefined) {
+      for (let j = 0; j < rest.length; j++) {
+        if (rest[j] !== i + 1) {
+          return [i + 1, rest[j]];
+        }
+      }
+    }
+  }
+};
