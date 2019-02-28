da27ac55a312ea135f04e9417767adffaa1a36ba
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-02-28 23:05:36 +0200

    Add solution for 69.

---
+++ solutions/0069.sqrtx.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {    
+  for (let i = 0; i <= x; i++) {
+    if (i ** 2 === x) {
+      return i;
+    } else if (i ** 2 > x) {
+      return i > 0 ? i - 1 : 0;
+    }
+  }
+};
