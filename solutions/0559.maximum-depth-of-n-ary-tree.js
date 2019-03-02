25399a22d485bbd2a62593baae5fed28f10e6068
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-03-02 16:46:54 +0200

    Add solution to 559.

---
+++ solutions/0559.maximum-depth-of-n-ary-tree.js
@@ -0,0 +1,25 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+/**
+ * @param {Node} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+  if (root === null) {
+    return 0;
+  } else if (root.children.length === 0) {
+    return 1;
+  } else {
+    const heights = [];
+
+    for (const child of root.children) {
+      heights.push(maxDepth(child));
+    }
+    return Math.max(...heights) + 1;
+  }
+};
