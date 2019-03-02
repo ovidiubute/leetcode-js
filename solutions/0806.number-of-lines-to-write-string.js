fece3f0b90c3d65ad826b99da7664f7c1e8f7f6b
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-03-02 17:56:08 +0200

    Add solution to 806.

---
+++ solutions/0806.number-of-lines-to-write-string.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} widths
+ * @param {string} S
+ * @return {number[]}
+ */
+var numberOfLines = function(widths, S) {
+  let lines = 1;
+  let lineWidth = 0;
+  for (let i = 0; i < S.length; i++) {
+    const w = widths[S.charCodeAt(i) - 97];
+
+    if (lineWidth + w > 100) {
+      lines++;
+      lineWidth = w;
+    } else {
+      lineWidth += w;
+    }
+  }
+
+  return [lines, lineWidth];
+};
