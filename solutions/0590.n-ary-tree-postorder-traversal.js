8e161968b04e2ad43e144333dd2de61c6527fe09
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-03-02 16:03:55 +0200

    Add solution to 590.

---
+++ solutions/0590.n-ary-tree-postorder-traversal.js
@@ -0,0 +1,29 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+/**
+ * @param {Node} root
+ * @return {number[]}
+ */
+var postorder = function(root) {
+  const results = [];
+  postorderHelper(root, results);
+  root && results.push(root.val);
+
+  return results;
+};
+
+var postorderHelper = function(root, results) {
+  if (root === null) {
+    return null;
+  }
+
+  for (const child of root.children) {
+    postorderHelper(child, results);
+    results.push(child.val);
+  }
+};
