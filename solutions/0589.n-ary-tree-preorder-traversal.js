8d6a680b0e1db738a493342aa75b1b8a492a30c3
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-03-02 15:56:17 +0200

    Add solution to 589.

---
+++ solutions/0589.n-ary-tree-preorder-traversal.js
@@ -0,0 +1,29 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+/**
+ * @param {Node} root
+ * @return {number[]}
+ */
+var preorder = function(root) {
+  const out = [];
+  pre(root, out);
+
+  return out;
+};
+
+var pre = function(root, out) {
+  if (root === null) {
+    return null;
+  }
+
+  out.push(root.val);
+
+  for (let child of root.children) {
+    pre(child, out);
+  }
+};
