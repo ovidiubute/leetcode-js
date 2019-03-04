81661ca1098559f420c9c68c66aa10c99355553f
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-03-04 23:31:23 +0200

    Add solution for 41.

---
+++ solutions/0041.first-missing-positive.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+  const seen = {};
+
+  let n = 1;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] < 0) continue;
+
+    seen[nums[i]] = true;
+
+    if (nums[i] === n) {
+      while (seen[n]) n++;
+    }
+  }
+
+  return n;
+};
