3e3062680d6804e05807629e70a19ff3ef4a925a
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-03-04 22:49:07 +0200

    Add solution for 118.

---
+++ solutions/0118.pascals-triangle.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+  const out = [];
+  for (let i = 0; i < numRows; i++) {
+    out[i] = [1];
+    out[i][i] = 1;
+
+    for (let j = 1; j < i; j++) {
+      out[i][j] = out[i - 1][j - 1] + out[i - 1][j];
+    }
+  }
+
+  return out;
+};
