29962fe696d9b42cf6edd77b052bac4b8c5186b3
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-03-07 16:41:42 +0200

    Add solution to 507.

---
+++ solutions/0507.perfect-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var checkPerfectNumber = function(num) {
+  if (num === 0) return false;
+
+  let sum = 0;
+  for (let i = 1; i < num; i++) {
+    if (num % i === 0) sum += i;
+
+    if (sum > num) return false;
+  }
+
+  return num === sum;
+};
