610e0a902487d59307087baaee1b34882e434a1d
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-03-07 17:34:07 +0200

    Add solution for 540.

---
+++ solutions/0540.single-element-in-a-sorted-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNonDuplicate = function(nums) {
+  for (let i = 1; i < nums.length; i += 2) {
+    if (nums[i - 1] !== nums[i]) {
+      return nums[i - 1];
+    }
+  }
+
+  return nums[nums.length - 1];
+};
