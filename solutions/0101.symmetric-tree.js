329eb92d4947a56a1705bc00a53351c8838e45c6
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-03-11 22:35:17 +0200

    Add solution for 101.

---
+++ solutions/0101.symmetric-tree.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+  return isMirror(root, root);
+};
+
+var isMirror = function(t1, t2) {
+  if (t1 === null && t2 === null) {
+    return true;
+  }
+
+  if (t1 === null || t2 === null) {
+    return false;
+  }
+
+  return (
+    t1.val === t2.val &&
+    isMirror(t1.left, t2.right) &&
+    isMirror(t1.right, t2.left)
+  );
+};
