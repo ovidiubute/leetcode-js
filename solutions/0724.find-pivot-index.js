230f2f797bb29311ed11c311152c4781bdc8731b
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-04-06 15:01:53 +0300

    Add solution to 724.

---
+++ solutions/0724.find-pivot-index.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+  let sum = 0;
+  let leftSum = 0;
+  
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+  }
+  
+  for (let i = 0; i < nums.length; i++) {
+    if (leftSum === sum - nums[i] - leftSum) {
+      return i;
+    }
+    
+    leftSum += nums[i];
+  }
+  
+  return -1;
+};
