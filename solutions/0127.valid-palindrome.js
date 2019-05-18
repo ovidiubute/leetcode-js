608916d50995fd5f6bc8d9bf55600cfc54f5e707
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-05-18 18:41:20 +0300

    Add solution to 127.

---
+++ solutions/0127.valid-palindrome.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+  return isPalindromeHelper(s.replace(/[^a-zA-Z0-9]/gi, "").toLowerCase());
+};
+
+var isPalindromeHelper = function(s) {
+  if (s.length < 2) return true;
+
+  if (s.charAt(0) !== s.charAt(s.length - 1)) return false;
+
+  return isPalindromeHelper(s.slice(1, s.length - 1));
+};
