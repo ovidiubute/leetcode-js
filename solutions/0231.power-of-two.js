794a3fbe82a10a5302df6a34550b5c3aa2fba282
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-05-18 18:47:20 +0300

    Add solution to 231.

---
+++ solutions/0231.power-of-two.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+  if (n === 1) {
+    return true;
+  }
+
+  if (n <= 0) {
+    return false;
+  }
+
+  let c = 1;
+  for (let i = 0; i < n; i++) {
+    c *= 2;
+
+    if (c === n) {
+      return true;
+    }
+
+    if (c > n) {
+      return false;
+    }
+  }
+
+  return false;
+};
--- update-badge.js
+++ update-badge.js
@@ -1,5 +1,5 @@
 // Update this over time from leetcode.com
-const TOTAL_PROBLEM_COUNT = 969;
+const TOTAL_PROBLEM_COUNT = 994;
 
 // Count how many solutions we have so far
 const countSolutionsScript = "ls solutions | wc -l";
