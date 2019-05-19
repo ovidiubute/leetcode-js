04b7bc4f02a30c0a0370afe4436c735f7bcd7914
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-05-19 11:36:03 +0300

    Add solution to 266.

---
+++ solutions/0266.palindrome-permutation.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var canPermutePalindrome = function(s) {
+  const o = {};
+
+  for (let i = 0; i < s.length; i++) {
+    if (o[s.charAt(i)] === undefined) {
+      o[s.charAt(i)] = 0;
+    }
+
+    o[s.charAt(i)]++;
+  }
+
+  let count = 0;
+  for (let [k, v] of Object.entries(o)) {
+    if (v % 2 === 1) {
+      count++;
+    }
+  }
+
+  return count <= 1;
+};
