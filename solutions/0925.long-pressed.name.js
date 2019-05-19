cce738d1e06e321a0278fa61dd979687dd1b16b3
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-05-19 12:13:33 +0300

    Add solution to 925.

---
+++ solutions/0925.long-pressed.name.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} name
+ * @param {string} typed
+ * @return {boolean}
+ */
+var isLongPressedName = function(name, typed) {
+  let i = 0,
+    j = 0;
+  while (i < name.length) {
+    const c = name.charAt(i);
+    const d = typed.charAt(j);
+
+    if (c === d) {
+      i++;
+      j++;
+      continue;
+    }
+
+    while (j < typed.length) {
+      if (c !== typed.charAt(j++)) {
+        break;
+      }
+    }
+
+    if (j === typed.length) {
+      return false;
+    }
+  }
+
+  return true;
+};
