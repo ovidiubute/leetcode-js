153d6dd6bba0a67e388736c91d704189c78cf44d
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-05-19 11:45:51 +0300

    Add solution to 326.

---
+++ solutions/0326.power-of-three.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+  if (n === 1) {
+    return true;
+  }
+
+  if (n === 0) {
+    return false;
+  }
+
+  while (n) {
+    n /= 3;
+
+    if (n === 1) {
+      return true;
+    }
+
+    if (n < 1) {
+      return false;
+    }
+  }
+};
