559eeb43f0b24e698b67ea815feb6ca279957b55
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-11-24 18:22:35 +0200

    Add solution to 1119.

---
+++ solutions/1119.remove-vowels-from-a-string.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} S
+ * @return {string}
+ */
+var removeVowels = function(S) {
+  let result = "";
+
+  for (let i = 0; i < S.length; i++) {
+    const c = S.charAt(i);
+    if (c === "a" || c === "e" || c === "i" || c === "o" || c === "u") continue;
+    result += c;
+  }
+
+  return result;
+};
