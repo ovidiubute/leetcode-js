d6f5ad6c1785ded25138738c96d81796aed2eb4d
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-11-25 09:40:03 +0200

    Add solution to 1165.

---
+++ solutions/1165.single-row-keyboard.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} keyboard
+ * @param {string} word
+ * @return {number}
+ */
+var calculateTime = function(keyboard, word) {
+  let time = 0;
+
+  if (word === "") return time;
+
+  if (word.length === 1) return keyboard.indexOf(word.charAt(0));
+
+  time += keyboard.indexOf(word.charAt(0));
+  for (let i = 1; i < word.length; i++) {
+    time += Math.abs(
+      keyboard.indexOf(word.charAt(i)) - keyboard.indexOf(word.charAt(i - 1))
+    );
+  }
+
+  return time;
+};
