13c7399b6b2730588061fba71e799b25e37390d2
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-12-25 17:46:06 +0200

    Add solution to 1295.

---
+++ solutions/1295.find-numbers-with-even-number-of-digits.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNumbers = function(nums) {
+  let total = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    const digits = digitCount(nums[i]);
+    if (digits % 2 === 0) {
+      total++;
+    }
+  }
+
+  return total;
+};
+
+var digitCount = function(num) {
+  let c = 0;
+
+  do {
+    num = Math.floor(num / 10);
+    c++;
+  } while (num);
+
+  return c;
+};
