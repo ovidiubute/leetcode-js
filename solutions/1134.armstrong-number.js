f244b27507fdfcab3320640c2df6d1eea9a12c5f
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-12-25 17:55:57 +0200

    Add solution to 1134.

---
+++ solutions/1134.armstrong-number.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} N
+ * @return {boolean}
+ */
+var isArmstrong = function(N) {
+  let result = 0;
+  let c = N;
+  const digits = [];
+
+  do {
+    digits.push(c % 10);
+    c = Math.floor(c / 10);
+  } while (c);
+
+  for (let i = 0; i < digits.length; i++) {
+    result += Math.pow(digits[i], digits.length);
+  }
+
+  return result === N;
+};
