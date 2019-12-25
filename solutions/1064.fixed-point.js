e8a7da29e805c4c71e3b5ab0c4e69e07e306349e
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-12-25 18:04:24 +0200

    Add solution to 1064.

---
+++ solutions/1064.fixed-point.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} A
+ * @return {number}
+ */
+var fixedPoint = function(A) {
+  for (let i = 0; i < A.length; i++) {
+    if (A[i] === i) return i;
+  }
+
+  return -1;
+};
