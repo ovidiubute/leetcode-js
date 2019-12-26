ace91675a86d7bed2d9f436d5e72c2a2e4c15a0f
Ovidiu Bute <ovidiu.bute@gmail.com>
2019-12-26 11:33:44 +0200

    Add solution to 1185.

---
+++ solutions/1185.day-of-the-week.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} day
+ * @param {number} month
+ * @param {number} year
+ * @return {string}
+ */
+var dayOfTheWeek = function(day, month, year) {
+  const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+
+  return weekdays[new Date(year, month - 1, day).getDay()];
+};
--- update-badge.js
+++ update-badge.js
@@ -1,5 +1,5 @@
 // Update this over time from leetcode.com
-const TOTAL_PROBLEM_COUNT = 1269;
+const TOTAL_PROBLEM_COUNT = 1196;
 
 // Count how many solutions we have so far
 const countSolutionsScript = "ls solutions | wc -l";
