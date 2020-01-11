b7cef43fa443d6859de21621c8b6fba8bf827cb8
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-01-11 17:11:06 +0200

    Add solution to 383.

---
+++ solutions/0383.ransom-note.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+  const letterRegistry = {};
+
+  for (let i = 0; i < magazine.length; i++) {
+    const letter = magazine.charAt(i);
+
+    if (letterRegistry[letter] !== undefined) {
+      letterRegistry[letter]++;
+    } else {
+      letterRegistry[letter] = 1;
+    }
+  }
+
+  for (let i = 0; i < ransomNote.length; i++) {
+    const letter = ransomNote.charAt(i);
+
+    if (!letterRegistry[letter]) {
+      return false;
+    }
+
+    letterRegistry[letter]--;
+  }
+
+  return true;
+};
