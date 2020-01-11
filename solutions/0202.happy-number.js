cf8d59ffe98aeacc9dbbdd93e3e2d3371d968826
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-01-11 16:38:50 +0200

    Add solution to 202.

---
+++ solutions/0202.happy-number.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+  let counter = 0;
+
+  do {
+    n = squareDigitsSum(n);
+
+    counter++;
+  } while (counter < 100 && n !== 1);
+
+  if (counter === 100) {
+    return false;
+  }
+
+  return true;
+};
+
+const squareDigitsSum = function(x) {
+  let s = 0;
+
+  while (x) {
+    const c = x % 10;
+    s += c ** 2;
+
+    x = Math.floor(x / 10);
+  }
+
+  return s;
+};
