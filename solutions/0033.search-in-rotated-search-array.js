a3ee408a628d73340a9ea8dfc8cfee7048cb8610
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-01-17 16:13:51 +0200

    Add solution to 33.

---
+++ solutions/0033.search-in-rotated-search-array.js
@@ -0,0 +1,68 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+  const findRotateIndex = function(left, right) {
+    if (nums[left] < nums[right]) {
+      return 0;
+    }
+
+    while (left <= right) {
+      const pivot = Math.floor((left + right) / 2);
+      if (nums[pivot] > nums[pivot + 1]) {
+        return pivot + 1;
+      } else {
+        if (nums[pivot] < nums[left]) {
+          right = pivot - 1;
+        } else {
+          left = pivot + 1;
+        }
+      }
+    }
+
+    return 0;
+  };
+
+  const binarySearch = function(left, right) {
+    while (left <= right) {
+      const pivot = Math.floor((left + right) / 2);
+      if (nums[pivot] === target) {
+        return pivot;
+      }
+
+      if (nums[pivot] > target) {
+        right = pivot - 1;
+      } else {
+        left = pivot + 1;
+      }
+    }
+
+    return -1;
+  };
+
+  if (nums.length === 0) {
+    return -1;
+  }
+
+  if (nums.length === 1) {
+    return nums[0] === target ? 0 : -1;
+  }
+
+  const rotateIndex = findRotateIndex(0, nums.length - 1);
+
+  if (nums[rotateIndex] === target) {
+    return rotateIndex;
+  }
+
+  if (rotateIndex === 0) {
+    return binarySearch(0, nums.length - 1);
+  }
+
+  if (target < nums[0]) {
+    return binarySearch(rotateIndex, nums.length - 1);
+  }
+
+  return binarySearch(0, rotateIndex);
+};
