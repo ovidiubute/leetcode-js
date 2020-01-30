d3c6e611c9c890f95d40bb5d426f651caca54d1a
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-01-30 23:34:08 +0200

    Add solution to 153.

---
+++ solutions/0153.find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i - 1] > nums[i]) {
+      return nums[i];
+    }
+  }
+
+  return nums[0];
+};
