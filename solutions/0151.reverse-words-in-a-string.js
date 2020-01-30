747a899f32b2670d192dc071f3a89492a6b97e27
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-01-30 09:39:23 +0200

    Add solution for 151.

---
+++ solutions/0151.reverse-words-in-a-string.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+  const input = s.split(" ");
+
+  if (input.length === 1) {
+    return input[0];
+  }
+
+  const words = [];
+  for (let i = input.length - 1; i >= 0; i--) {
+    if (input[i] !== "") words.push(input[i]);
+  }
+
+  return words.join(" ");
+};
