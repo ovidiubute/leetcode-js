33d53fed1d77abd50d6f7fb10678ee328bfb4a67
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-01-30 08:48:04 +0200

    Add solution for 73.

---
+++ solutions/0073.set-matrix-zeroes.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function(matrix) {
+  const zLines = {};
+  const zColumns = {};
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === 0) {
+        zLines[i] = 1;
+        zColumns[j] = 1;
+      }
+    }
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (zLines[i] || zColumns[j]) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+};
