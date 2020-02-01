0443353c15553b75dea3f2ef779296437d269423
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-02-01 12:39:49 +0200

    Add solution to 144.

---
+++ solutions/0144.binary-tree-preorder-traversal.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+  if (root === null) {
+    return [];
+  }
+
+  const stack = [];
+  const output = [];
+
+  stack.push(root);
+  while (stack.length) {
+    const node = stack.pop();
+    output.push(node.val);
+
+    if (node.right) {
+      stack.push(node.right);
+    }
+
+    if (node.left) {
+      stack.push(node.left);
+    }
+  }
+
+  return output;
+};
