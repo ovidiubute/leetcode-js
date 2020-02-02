bb57c99a1e2b303089d4f45f636253a707240e3f
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-02-02 16:08:46 +0200

    Add solution to 1323.

---
+++ solutions/1323.maximum-69-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var maximum69Number = function(num) {
+  const s = String(num);
+
+  for (let i = 0; i < s.length; i++) {
+    if (s.charAt(i) === "6") {
+      return Number.parseInt(s.slice(0, i) + "9" + s.slice(i + 1));
+    }
+  }
+
+  return num;
+};
