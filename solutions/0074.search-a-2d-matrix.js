b790f5f4f1b2cd7cc3b45572e1001d4c0bfdd221
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-02-02 16:48:17 +0200

    Add solution to 74.

---
+++ solutions/0074.search-a-2d-matrix.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+  let arr = [];
+  for (let i = 0; i < matrix.length; i++) {
+    arr = arr.concat(matrix[i]);
+  }
+
+  let lo = 0,
+    hi = arr.length - 1;
+  while (lo <= hi) {
+    const mid = Math.floor((lo + hi) / 2);
+
+    if (arr[mid] === target) {
+      return true;
+    }
+
+    if (arr[mid] > target) {
+      hi = mid - 1;
+    } else {
+      lo = mid + 1;
+    }
+  }
+
+  return false;
+};
