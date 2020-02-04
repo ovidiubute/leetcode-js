1f216222199558814a002854e87de05a1d6fb92d
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-02-04 09:45:54 +0200

    Add solution to 179.

---
+++ solutions/0179.largest-number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var largestNumber = function(nums) {
+  nums.sort((a, b) => {
+    const x = String(a) + String(b);
+    const y = String(b) + String(a);
+
+    return x > y ? -1 : x < y ? 1 : 0;
+  });
+
+  if (nums[0] === 0) {
+    return "0";
+  }
+
+  return nums.join("");
+};
