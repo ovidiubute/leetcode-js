763d54c402742a4056042e33e36c30254e248a8c
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-02-04 10:19:56 +0200

    Add solution to 228.

---
+++ solutions/0228.summary-ranges.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function(nums) {
+  if (!nums.length) {
+    return [];
+  }
+
+  const out = [];
+  let prev = nums[0];
+  out.push(`${prev}`);
+  for (let i = 1; i < nums.length; i++) {
+    if (Math.abs(nums[i] - nums[i - 1]) !== 1) {
+      prev = nums[i];
+      out.push(`${prev}`);
+    } else {
+      out[out.length - 1] = `${prev}->${nums[i]}`;
+    }
+  }
+
+  return out;
+};
