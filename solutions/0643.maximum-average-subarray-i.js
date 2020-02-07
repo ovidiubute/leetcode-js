9b9893c69c5004c87da75b6fcd1704479f299068
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-02-07 20:06:01 +0200

    Add solution to 643.

---
+++ solutions/0643.maximum-average-subarray-i.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function(nums, k) {
+  let max = -Number.MAX_SAFE_INTEGER;
+
+  let i = 0,
+    j = k - 1;
+
+  let s = 0;
+  for (let q = i; q <= j; q++) {
+    s += nums[q];
+  }
+
+  while (j < nums.length) {
+    if (i > 0) {
+      s -= nums[i - 1];
+      s += nums[j];
+    }
+
+    let avg = s / k;
+    if (avg > max) {
+      max = avg;
+    }
+
+    i++;
+    j++;
+  }
+
+  return max;
+};
