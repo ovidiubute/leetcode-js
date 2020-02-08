084259c93396645862721daf13e8e15b3911e051
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-02-08 17:39:45 +0200

    Add solution to 702.

---
+++ solutions/0702.search-in-a-sorted-array-of-unknown-size.js
@@ -0,0 +1,18 @@
+/**
+ * @param {ArrayReader} reader
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(reader, target) {
+  let i = 0;
+
+  while (reader.get(i) !== 2147483647) {
+    if (reader.get(i) === target) {
+      return i;
+    }
+
+    i++;
+  }
+
+  return -1;
+};
