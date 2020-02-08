01e55567ca5394759ee6ff612498526840e29bb8
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-02-08 16:29:27 +0200

    Add solution to 645.

---
+++ solutions/0645.set-mismatch.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findErrorNums = function(nums) {
+  const seen = {};
+  const out = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (seen[nums[i]] === undefined) {
+      seen[nums[i]] = 1;
+    } else {
+      out[0] = nums[i];
+    }
+  }
+
+  for (let i = 1; i <= nums.length; i++) {
+    if (seen[i] === undefined) {
+      out[1] = i;
+    }
+  }
+
+  return out;
+};
