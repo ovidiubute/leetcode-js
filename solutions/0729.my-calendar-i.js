1f8b0f3c16683775aa184285bfb9bd1fb7b48ae0
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-02-09 10:15:39 +0200

    Add solution to 729.

---
+++ solutions/0729.my-calendar-i.js
@@ -0,0 +1,26 @@
+var MyCalendar = function() {
+  this.data = [];
+};
+
+/**
+ * @param {number} start
+ * @param {number} end
+ * @return {boolean}
+ */
+MyCalendar.prototype.book = function(start, end) {
+  for (let i = 0; i < this.data.length; i++) {
+    if (this.data[i][0] < end && start < this.data[i][1]) {
+      return false;
+    }
+  }
+
+  this.data.push([start, end]);
+
+  return true;
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * var obj = new MyCalendar()
+ * var param_1 = obj.book(start,end)
+ */
