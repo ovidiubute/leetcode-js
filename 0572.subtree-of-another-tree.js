b9005c2550b49425bef4c3627a2acdfb74f5ec47
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-02-09 16:03:05 +0200

    Add solution to 572.

---
+++ 0572.subtree-of-another-tree.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} s
+ * @param {TreeNode} t
+ * @return {boolean}
+ */
+var isSubtree = function(s, t) {
+  return dfs(s, "left").indexOf(dfs(t, "left")) !== -1;
+};
+
+var dfs = function(node, side) {
+  if (node === null) {
+    if (side === "left") {
+      return "leftNull";
+    } else {
+      return "rightNull";
+    }
+  }
+
+  return (
+    "$" +
+    node.val +
+    " " +
+    dfs(node.left, "left") +
+    " " +
+    dfs(node.right, "right")
+  );
+};
