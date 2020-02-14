a658706754313fea7cf2444ba820abcce6d8a229
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-02-14 10:01:30 +0200

    Add solution to 24.

---
+++ solutions/0024.swap-nodes-in-pairs.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+  const h = new ListNode(null);
+  h.next = head;
+
+  let prev = h;
+
+  while (head && head.next) {
+    let p = head;
+    let q = head.next;
+
+    prev.next = q;
+    p.next = q.next;
+    q.next = p;
+
+    prev = p;
+    head = p.next;
+  }
+
+  return h.next;
+};
