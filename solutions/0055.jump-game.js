a2cd9074e752959a67ffffb5a078322ae8098cdb
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-02-15 16:01:18 +0200

    Add solution to 55.

---
+++ solutions/0055.jump-game.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+  const memo = new Array(nums.length);
+
+  for (let i = 0; i < memo.length; i++) {
+    memo[i] = -1;
+  }
+  memo[memo.length - 1] = 1;
+
+  for (let i = nums.length - 2; i >= 0; i--) {
+    let maxJump = Math.min(i + nums[i], nums.length - 1);
+
+    for (let j = i + 1; j <= maxJump; j++) {
+      if (memo[j] === 1) {
+        memo[i] = 1;
+        break;
+      }
+    }
+  }
+
+  return memo[0] === 1;
+};
