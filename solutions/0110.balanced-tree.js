1110bc5ac769bbce8371b9787038557e7cb2daf0
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-02-15 16:28:11 +0200

    Add solution to 110.

---
+++ solutions/0110.balanced-tree.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+  if (root === null) {
+    return true;
+  }
+
+  if (Math.abs(height(root.left) - height(root.right)) > 1) {
+    return false;
+  } else {
+    return isBalanced(root.left) && isBalanced(root.right);
+  }
+};
+
+const height = function(node) {
+  if (node === null) {
+    return -1;
+  }
+
+  return 1 + Math.max(height(node.left), height(node.right));
+};
