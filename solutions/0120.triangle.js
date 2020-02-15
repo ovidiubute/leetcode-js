ff587aad0f3b1815f931581c3d11b3e684a39fc0
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-02-15 19:01:16 +0200

    Add solution to 120.

---
+++ solutions/0120.triangle.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[][]} triangle
+ * @return {number}
+ */
+var minimumTotal = function(triangle) {
+  if (!triangle.length) return 0;
+
+  let dp = new Array(triangle.length);
+  for (let i = 0; i < dp.length; i++) {
+    dp[i] = 0;
+  }
+  dp[0] = triangle[0][0];
+
+  for (let i = 1; i < triangle.length; ++i) {
+    for (let j = triangle[i].length - 1; j >= 0; --j) {
+      if (j === 0) {
+        dp[j] = dp[j] + triangle[i][j];
+      } else if (j === triangle[i].length - 1) {
+        dp[j] = dp[j - 1] + triangle[i][j];
+      } else {
+        dp[j] = Math.min(dp[j], dp[j - 1]) + triangle[i][j];
+      }
+    }
+  }
+
+  return Math.min(...dp);
+};
