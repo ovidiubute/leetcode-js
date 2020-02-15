cd119d2788d8da556050c09afb4d55130b0ec474
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-02-15 16:42:59 +0200

    Add solution to 111.

---
+++ solutions/0111.minimum-depth-of-binary-tree.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function(root) {
+  if (root === null) {
+    return 0;
+  }
+
+  if (!root.left && !root.right) {
+    return 1;
+  }
+
+  let minimumDepth = Number.MAX_SAFE_INTEGER;
+
+  if (root.left != null) {
+    minimumDepth = Math.min(minDepth(root.left), minimumDepth);
+  }
+  if (root.right != null) {
+    minimumDepth = Math.min(minDepth(root.right), minimumDepth);
+  }
+
+  return minimumDepth + 1;
+};
