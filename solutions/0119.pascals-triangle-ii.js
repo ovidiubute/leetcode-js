1e3b539f0999364abe295b4368d611eae7af85fc
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-02-15 18:19:41 +0200

    Add solution to 119.

---
+++ solutions/0119.pascals-triangle-ii.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function(rowIndex) {
+  const out = new Array(rowIndex + 1);
+  const mid = Math.round(out.length / 2);
+
+  for (let i = 0; i < mid; i++) {
+    const value = combinations(rowIndex, i);
+    out[i] = value;
+    out[out.length - 1 - i] = value;
+  }
+
+  return out;
+};
+
+const memo = {};
+
+const combinations = function(n, k) {
+  let nFact = memo[n] ? memo[n] : fact(n);
+  let kFact = memo[k] ? memo[k] : fact(k);
+  let nkFact = memo[n - k] ? memo[n - k] : fact(n - k);
+
+  return nFact / (kFact * nkFact);
+};
+
+const fact = function(n) {
+  if (n === 0) {
+    return 1;
+  }
+
+  return n * fact(n - 1);
+};
