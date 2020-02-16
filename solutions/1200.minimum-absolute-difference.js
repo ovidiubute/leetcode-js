a0bb277a36a83e298313dde705806b3cab68ee18
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-02-16 14:13:41 +0200

    Add solution to 1200.

---
+++ solutions/1200.minimum-absolute-difference.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} arr
+ * @return {number[][]}
+ */
+var minimumAbsDifference = function(arr) {
+  arr.sort((a, b) => {
+    return a > b ? 1 : -1;
+  });
+
+  let pairs = [[arr[0], arr[1]]];
+  let min = Math.abs(arr[0] - arr[1]);
+  for (let i = 1; i < arr.length; i++) {
+    const diff = Math.abs(arr[i] - arr[i + 1]);
+
+    if (diff === min) {
+      pairs.push([arr[i], arr[i + 1]]);
+    }
+
+    if (min > diff) {
+      pairs = [[arr[i], arr[i + 1]]];
+      min = diff;
+    }
+  }
+
+  return pairs;
+};
