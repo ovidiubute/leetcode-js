f8b821c49f119fe6789b2eae99076a325ed499d9
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-02-16 11:32:29 +0200

    Add solution to 1221.

---
+++ solutions/1221.split-a-string-in-balanced-strings.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var balancedStringSplit = function(s) {
+  let strings = 0,
+    lcount = 0,
+    rcount = 0,
+    i = 0;
+
+  while (i <= s.length) {
+    if (s.charAt(i) === "L") lcount++;
+    else rcount++;
+    i++;
+
+    if (lcount === rcount) {
+      strings++;
+      lcount = 0;
+      rcount = 0;
+    }
+  }
+
+  return strings;
+};
