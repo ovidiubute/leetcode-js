5bc14e4b4f670995286d1a9a444b5733e2948808
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-02-17 00:18:54 +0200

    Add solution to 345.

---
+++ solutions/0345.reverse-vowels-of-a-string.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function(s) {
+  let i = 0,
+    j = s.length - 1,
+    out = [...s],
+    v = new Set(["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]);
+
+  while (i < j) {
+    while (i < j && !v.has(s.charAt(i))) {
+      i++;
+    }
+
+    while (i < j && !v.has(s.charAt(j))) {
+      j--;
+    }
+
+    out[i] = s.charAt(j);
+    out[j] = s.charAt(i);
+    i++;
+    j--;
+  }
+
+  return out.join("");
+};
