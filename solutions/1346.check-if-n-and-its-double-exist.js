d01162dddb33ae51cb5838e08befd9190e5dfd57
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-02-16 11:02:54 +0200

    Add solution to 1346.

---
+++ solutions/1346.check-if-n-and-its-double-exist.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var checkIfExist = function(arr) {
+  const d = arr.map(x => x * 2);
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < d.length; j++) {
+      if (i !== j && arr[i] === d[j]) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+};
