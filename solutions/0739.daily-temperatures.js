352c65bbb39f6cc5561decd6f69d289e01100e87
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-02-22 17:51:22 +0200

    Add solution to 739.

---
+++ solutions/0739.daily-temperatures.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} T
+ * @return {number[]}
+ */
+var dailyTemperatures = function(T) {
+  const out = [];
+  for (let i = 0; i < T.length; i++) {
+    let j = i + 1;
+    while (T[j] <= T[i] && j < T.length) {
+      j++;
+    }
+
+    if (T[j] > T[i]) {
+      out[i] = j - i;
+    } else {
+      out[i] = 0;
+    }
+  }
+
+  return out;
+};
