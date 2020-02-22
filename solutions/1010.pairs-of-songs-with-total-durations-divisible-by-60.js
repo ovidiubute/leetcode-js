ab3d7b3532565fd29d3e4084416cce00104497c1
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-02-22 21:25:02 +0200

    Add solution to 1010.

---
+++ solutions/1010.pairs-of-songs-with-total-durations-divisible-by-60.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} time
+ * @return {number}
+ */
+var numPairsDivisibleBy60 = function(time) {
+  let count = 0;
+
+  for (let i = 0; i < time.length - 1; i++) {
+    for (let j = i + 1; j < time.length; j++) {
+      if ((time[i] + time[j]) % 60 === 0) count++;
+    }
+  }
+
+  return count;
+};
