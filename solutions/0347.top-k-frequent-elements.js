703aad89413f91cbcdabaa065853b6a4355bf28e
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-02-22 09:31:21 +0200

    Add solution to 347.

---
+++ solutions/0347.top-k-frequent-elements.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+  const freq = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (!freq.has(nums[i])) {
+      freq.set(nums[i], 1);
+    } else {
+      const val = freq.get(nums[i]);
+      freq.set(nums[i], val + 1);
+    }
+  }
+
+  pqueue = [];
+  for (let [k, v] of freq) {
+    pqueue.push({ value: k, priority: v });
+  }
+
+  pqueue.sort((a, b) => {
+    return a.priority < b.priority ? -1 : a.priority > b.priority ? 1 : 0;
+  });
+
+  const result = [];
+  for (let i = pqueue.length - 1; i >= pqueue.length - k; i--) {
+    result.push(pqueue[i].value);
+  }
+
+  return result;
+};
