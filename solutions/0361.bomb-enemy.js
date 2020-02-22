39a8427ddf87bc4ec0809735d627b5590c4eee4d
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-02-22 10:08:39 +0200

    Add solution to 361.

---
+++ solutions/0361.bomb-enemy.js
@@ -0,0 +1,44 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var maxKilledEnemies = function(grid) {
+  let max = 0;
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] !== "0") continue;
+      let kills = 0;
+
+      for (let k = i; k >= 0; k--) {
+        if (grid[k][j] === "W") break;
+
+        if (grid[k][j] === "E") kills++;
+      }
+
+      for (let k = i + 1; k < grid.length; k++) {
+        if (grid[k][j] === "W") break;
+
+        if (grid[k][j] === "E") kills++;
+      }
+
+      for (let k = j; k >= 0; k--) {
+        if (grid[i][k] === "W") break;
+
+        if (grid[i][k] === "E") kills++;
+      }
+
+      for (let k = j + 1; k < grid[i].length; k++) {
+        if (grid[i][k] === "W") break;
+
+        if (grid[i][k] === "E") kills++;
+      }
+
+      if (kills > max) {
+        max = kills;
+      }
+    }
+  }
+
+  return max;
+};
