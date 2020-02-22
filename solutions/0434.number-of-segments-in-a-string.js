98397a6090be6cf9ba5e05ad2f34fd277f582f77
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-02-22 16:02:08 +0200

    Add solution to 434.

---
+++ solutions/0434.number-of-segments-in-a-string.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSegments = function(s) {
+  let count = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s.charAt(i) !== " " && (i === 0 || s.charAt(i - 1) === " ")) {
+      count++;
+    }
+  }
+
+  return count;
+};
