17f1202232a497510139eebe651f406d5b38ae2b
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-02-22 09:39:23 +0200

    Add solution to 342.

---
+++ solutions/0342.power-of-four.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPowerOfFour = function(num) {
+  while (num !== 1 && Math.floor(num) !== 0) {
+    num /= 4;
+  }
+
+  return num === 1;
+};
