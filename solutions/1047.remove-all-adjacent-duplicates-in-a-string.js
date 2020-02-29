9d5ae07de790db6908dca304fd2a78f7ce4c6cc4
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-02-29 17:21:48 +0200

    Add solution to 1047.

---
+++ solutions/1047.remove-all-adjacent-duplicates-in-a-string.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} S
+ * @return {string}
+ */
+var removeDuplicates = function(S) {
+  const stack = [];
+
+  for (let i = 0; i < S.length; i++) {
+    if (S.charAt(i) === stack[stack.length - 1]) {
+      stack.pop();
+    } else {
+      stack.push(S.charAt(i));
+    }
+  }
+
+  return stack.join("");
+};
