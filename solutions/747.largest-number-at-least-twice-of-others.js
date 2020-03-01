21584b3f7dd90a931185a78e9af4a8518016cb76
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-03-01 14:08:18 +0200

    Add solution to 747.

---
+++ solutions/747.largest-number-at-least-twice-of-others.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var dominantIndex = function(nums) {
+  let max = (pos = -1);
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > max) {
+      max = nums[i];
+      pos = i;
+    }
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0 && i !== pos && Math.floor(max / nums[i]) < 2) {
+      return -1;
+    }
+  }
+
+  return pos;
+};
