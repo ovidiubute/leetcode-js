be07f8bf6862c83e0761f6286c12193624e7397a
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-03-01 13:32:27 +0200

    Add solution to 1154.

---
+++ solutions/1154.day-of-the-year.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} date
+ * @return {number}
+ */
+var dayOfYear = function(date) {
+  const daysEachMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+  const [y, m, d] = date.split("-").map(x => Number.parseInt(x, 10));
+
+  if (y % 400 === 0 || (y % 100 !== 0 && y % 4 === 0)) {
+    daysEachMonth[1] = 29;
+  }
+
+  let days = 0;
+  for (let i = 0; i < m - 1; i++) {
+    days += daysEachMonth[i];
+  }
+
+  return days + d;
+};
