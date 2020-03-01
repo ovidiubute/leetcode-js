b781b270c9a26fbe5c11b4efd025f30d7dbf4dc2
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-03-01 13:53:41 +0200

    Add solution to 868.

---
+++ solutions/0868.binary-gap.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} N
+ * @return {number}
+ */
+var binaryGap = function(N) {
+  const binaryN = N.toString(2);
+
+  let prevIndex = -1;
+  let dist = 0;
+  for (let i = 0; i < binaryN.length; i++) {
+    if (binaryN.charAt(i) === "1") {
+      dist = Math.max(dist, prevIndex > -1 ? i - prevIndex : i);
+      prevIndex = i;
+    }
+  }
+
+  return dist;
+};
