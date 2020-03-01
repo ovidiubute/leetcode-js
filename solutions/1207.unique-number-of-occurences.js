1c9b00e27b39416d5c95f214a3275712887f8c5a
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-03-01 12:04:44 +0200

    Add solution to 1207.

---
+++ solutions/1207.unique-number-of-occurences.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function(arr) {
+  const freq = new Map();
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!freq.has(arr[i])) {
+      freq.set(arr[i], 1);
+    } else {
+      freq.set(arr[i], freq.get(arr[i]) + 1);
+    }
+  }
+
+  const set = new Set();
+  for (let value of freq.values()) {
+    if (set.has(value)) {
+      return false;
+    }
+
+    set.add(value);
+  }
+
+  return true;
+};
