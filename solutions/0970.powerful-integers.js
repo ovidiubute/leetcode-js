4af90c28e0782533d2e191bee78ddff9a6087c2d
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-03-02 21:33:27 +0200

    Add solution to 970.

---
+++ solutions/0970.powerful-integers.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @param {number} bound
+ * @return {number[]}
+ */
+var powerfulIntegers = function(x, y, bound) {
+  const out = new Set();
+
+  for (let i = 0; i < 20 && x ** i <= bound; i++) {
+    for (let j = 0; j < 20 && x ** i + y ** j <= bound; j++) {
+      out.add(x ** i + y ** j);
+    }
+  }
+
+  return Array.from(out.values());
+};
