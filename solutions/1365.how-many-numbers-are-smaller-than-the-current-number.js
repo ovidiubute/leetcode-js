1dc4b51fe2e08b3555c2a2d31b31690529e2bad1
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-03-02 21:40:40 +0200

    Add solution to 1365.

---
+++ solutions/1365.how-many-numbers-are-smaller-than-the-current-number.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var smallerNumbersThanCurrent = function(nums) {
+  const out = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    out[i] = 0;
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (i !== j && nums[i] > nums[j]) {
+        out[i]++;
+      }
+    }
+  }
+
+  return out;
+};
