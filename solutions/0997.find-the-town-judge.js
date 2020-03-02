eef8cfd35bce1521b922a34435179e17b71e1dae
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-03-02 09:23:38 +0200

    Add solution to 997.

---
+++ solutions/0997.find-the-town-judge.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} N
+ * @param {number[][]} trust
+ * @return {number}
+ */
+var findJudge = function(N, trust) {
+  const votes = new Array(N);
+  for (let i = 0; i < votes.length; i++) {
+    votes[i] = 0;
+  }
+
+  for (let i = 0; i < trust.length; i++) {
+    votes[trust[i][1] - 1]++;
+  }
+
+  for (let i = 0; i < votes.length; i++) {
+    if (votes[i] === N - 1) {
+      let trustsNobody = true;
+      for (let j = 0; j < trust.length; j++) {
+        if (trust[j][0] === i + 1) {
+          trustsNobody = false;
+          break;
+        }
+      }
+
+      if (trustsNobody) {
+        return i + 1;
+      }
+    }
+  }
+
+  return -1;
+};
