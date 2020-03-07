aa3fdd9641020083011eba642aac18a9b795c1ee
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-03-07 17:31:09 +0200

    Add solution to 1056.

---
+++ solutions/1056.confusing-number.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number} N
+ * @return {boolean}
+ */
+var confusingNumber = function(N) {
+  const chars = String(N)
+    .split("")
+    .reverse();
+  const mirrorChars = {
+    "1": "1",
+    "2": "x",
+    "3": "x",
+    "4": "x",
+    "5": "x",
+    "6": "9",
+    "7": "x",
+    "8": "8",
+    "9": "6",
+    "0": "0"
+  };
+
+  let newNumber = "";
+  for (let i = 0; i < chars.length; i++) {
+    if (mirrorChars[chars[i]] === "x") {
+      return false;
+    }
+
+    newNumber += mirrorChars[chars[i]];
+  }
+
+  if (Number(newNumber) === N) {
+    return false;
+  }
+
+  return true;
+};
