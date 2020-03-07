e28836dafd0e4dcc69af9818ef684198c5575050
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-03-07 16:00:31 +0200

    Add solution to 830.

---
+++ solutions/0830.positions-of-large-groups.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} S
+ * @return {number[][]}
+ */
+var largeGroupPositions = function(S) {
+  const out = [];
+  let prev = "",
+    count = 0,
+    i = 0,
+    startIdx = 0;
+
+  while (i < S.length) {
+    let c = S.charAt(i);
+
+    if (c !== prev) {
+      prev = c;
+
+      if (count > 2) {
+        out.push([startIdx, i - 1]);
+      }
+
+      startIdx = i;
+      count = 1;
+    } else {
+      count++;
+    }
+
+    i++;
+  }
+
+  if (count > 2) {
+    out.push([startIdx, i - 1]);
+  }
+
+  return out;
+};
