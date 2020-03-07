efa09626c96920060e3a7fc0b98df9ec7976f8dc
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-03-07 18:02:14 +0200

    Add solution to 1089.

---
+++ solutions/1089.duplicate-zeroes.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+var duplicateZeros = function(arr) {
+  const indexes = [];
+  const n = arr.length;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      indexes.push(i);
+    }
+  }
+
+  for (let i = indexes.length - 1; i >= 0; i--) {
+    arr.splice(indexes[i], 0, 0);
+  }
+
+  if (n < arr.length) {
+    const diff = arr.length - n;
+    arr.splice(arr.length - diff, diff);
+  }
+};
