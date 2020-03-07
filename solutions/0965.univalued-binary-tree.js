2f4c2a22bbbb275eb0352dad190ec7aeeb90da7c
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-03-07 16:31:20 +0200

    Add solution to 965.

---
+++ solutions/0965.univalued-binary-tree.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isUnivalTree = function(root) {
+  if (root === null) {
+    return true;
+  }
+
+  const v = root.val;
+
+  return helper(root, v);
+};
+
+var helper = function(node, value) {
+  if (node === null) {
+    return true;
+  }
+
+  const left = helper(node.left, value);
+
+  if (!left) {
+    return false;
+  }
+
+  const right = helper(node.right, value);
+
+  if (!right) {
+    return false;
+  }
+
+  if (node.val !== value) {
+    return false;
+  }
+
+  return true;
+};
