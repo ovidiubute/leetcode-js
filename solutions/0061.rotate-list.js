617ac6b09437e7d376e38b97c9d475dbf0221e6d
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-03-08 14:16:58 +0200

    Add solution to 61.

---
+++ solutions/0061.rotate-list.js
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var rotateRight = function(head, k) {
+  if (head === null) {
+    return null;
+  }
+  
+  if (head.next === null) {
+    return head;
+  }
+  
+  let t = new ListNode(null);
+  t.next = head;
+  
+  let count = 0;
+  let p = t.next;
+  while (p) {
+    p = p.next;
+    count++;
+  }
+
+  let q = k;
+  if (count < q) {
+    q = k % count;
+  }
+  
+  while (q) {
+    let p = t.next;
+    while (p.next.next) {
+      p = p.next;
+    }
+
+    p.next.next = t.next;
+    t.next = p.next;
+    p.next = null;
+    
+    q--;
+  }
+  
+  return t.next;
+};
