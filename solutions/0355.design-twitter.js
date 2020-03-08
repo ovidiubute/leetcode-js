e457f528d8319a99a221c1f8420eb9aa568f12f2
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-03-08 15:03:04 +0200

    Add solution to 355.

---
+++ solutions/0355.design-twitter.js
@@ -0,0 +1,114 @@
+/**
+ * Initialize your data structure here.
+ */
+var Twitter = function() {
+  this.followersByUser = new Map();
+  this.tweetsByUser = new Map();
+
+  // performance.now() is not available in LeetCode, and Date.now()
+  // is not accurate enough between API calls
+  this.time = 0;
+};
+
+/**
+ * Compose a new tweet.
+ * @param {number} userId
+ * @param {number} tweetId
+ * @return {void}
+ */
+Twitter.prototype.postTweet = function(userId, tweetId) {
+  let tweets;
+  if (this.tweetsByUser.has(userId)) {
+    tweets = this.tweetsByUser.get(userId);
+  } else {
+    tweets = new Map();
+  }
+
+  tweets.set(++this.time, tweetId);
+  this.tweetsByUser.set(userId, tweets);
+};
+
+/**
+ * Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent.
+ * @param {number} userId
+ * @return {number[]}
+ */
+Twitter.prototype.getNewsFeed = function(userId) {
+  let users = [userId];
+
+  if (this.followersByUser.has(userId)) {
+    if (this.followersByUser.get(userId).size) {
+      users = users.concat(Array.from(this.followersByUser.get(userId)));
+    }
+  }
+
+  let tweets = [];
+
+  for (let user of users) {
+    if (!this.tweetsByUser.has(user)) {
+      continue;
+    }
+
+    const userTweets = this.tweetsByUser.get(user);
+    for (let [ts, tweetId] of userTweets) {
+      tweets.push({
+        ts,
+        tweetId
+      });
+    }
+  }
+
+  tweets.sort((a, b) => {
+    if (a.ts > b.ts) {
+      return -1;
+    } else if (a.ts < b.ts) {
+      return 1;
+    }
+
+    return 0;
+  });
+
+  return tweets.map(t => t.tweetId).slice(0, 10);
+};
+
+/**
+ * Follower follows a followee. If the operation is invalid, it should be a no-op.
+ * @param {number} followerId
+ * @param {number} followeeId
+ * @return {void}
+ */
+Twitter.prototype.follow = function(followerId, followeeId) {
+  if (followerId === followeeId) {
+    return;
+  }
+
+  if (this.followersByUser.has(followerId)) {
+    this.followersByUser.get(followerId).add(followeeId);
+  } else {
+    this.followersByUser.set(followerId, new Set([followeeId]));
+  }
+};
+
+/**
+ * Follower unfollows a followee. If the operation is invalid, it should be a no-op.
+ * @param {number} followerId
+ * @param {number} followeeId
+ * @return {void}
+ */
+Twitter.prototype.unfollow = function(followerId, followeeId) {
+  if (this.followersByUser.has(followerId)) {
+    const followers = this.followersByUser.get(followerId);
+    followers.delete(followeeId);
+
+    this.followersByUser.set(followerId, followers);
+  }
+};
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * var obj = new Twitter()
+ * obj.postTweet(userId,tweetId)
+ * var param_2 = obj.getNewsFeed(userId)
+ * obj.follow(followerId,followeeId)
+ * obj.unfollow(followerId,followeeId)
+ */
