282bb1fe7775fc106afe8c23d866b430c900c6e6
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-03-12 18:30:09 +0200

    Add solution to 336.

---
+++ solutions/0336.palindrome-pairs.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string[]} words
+ * @return {number[][]}
+ */
+var palindromePairs = function(words) {
+  const results = [];
+
+  for (let i = 0; i < words.length; i++) {
+    for (let j = 0; j < words.length; j++) {
+      if (i === j) {
+        continue;
+      }
+
+      let p = words[i] + words[j];
+
+      let k = 0,
+        m = p.length - 1;
+      if (p.charAt(k) !== p.charAt(m)) {
+        continue;
+      }
+
+      while (k < m && p.charAt(k) === p.charAt(m)) {
+        k++;
+        m--;
+      }
+
+      if (p.charAt(k) === p.charAt(m) && k - m <= 1) {
+        results.push([i, j]);
+      }
+    }
+  }
+
+  return results;
+};
