df850c9de8aac6f01c3f6b6dfb79bfb64008a2ed
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-03-15 09:40:33 +0200

    Add solution to 392.

---
+++ solutions/0392.is-subsequence.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+  let i = 0,
+    j = 0;
+
+  while (i < s.length && j < t.length) {
+    if (s[i] === t[j]) {
+      i++;
+      j++;
+    } else {
+      j++;
+    }
+  }
+
+  return i === s.length;
+};
