c043b97f38393152e901bc176d1938436c6fbde8
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-03-15 14:37:14 +0200

    Add solution to 204.

---
+++ solutions/0204.count-primes.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+  let count = 0;
+
+  for (let i = 2; i < n; i++) {
+    if (isPrime(i)) count++;
+  }
+
+  return count;
+};
+
+var isPrime = function(x, memo) {
+  const sqrt = Math.floor(Math.sqrt(x));
+  for (let i = 2; i <= sqrt; i++) {
+    if (x % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
