90be534d86fc282e6d821cead7c3d2b5f064e64c
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-03-15 14:44:23 +0200

    Add solution to 234.

---
+++ solutions/0234.palindrome-linked-list.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+  const list = [];
+
+  let p = head;
+  while (p) {
+    list.push(p.val);
+    p = p.next;
+  }
+
+  let i = 0,
+    j = list.length - 1;
+  while (i < j && list[i] === list[j]) {
+    i++;
+    j--;
+  }
+
+  return list[i] === list[j] && j - i <= 1;
+};
