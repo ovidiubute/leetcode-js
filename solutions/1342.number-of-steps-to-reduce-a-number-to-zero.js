7e74dc4306ab6e4a94fa0bde0dbcce8020301a5a
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-10-07 08:45:51 +0000

    Add solution to 1342.

---
+++ solutions/1342.number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function (num) {
+  let count = 0;
+
+  while (num > 0) {
+    if (num % 2 === 0) {
+      num /= 2;
+    } else {
+      num -= 1;
+    }
+
+    count++
+  }
+
+  return count;
+};
