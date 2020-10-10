5f813dd16b2dda9a5a2ad8d463be91b2f9e0f2a8
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-10-10 15:59:39 +0000

    Add solution to 408.

---
+++ solutions/0408.valid-word-abbreviation.js
@@ -0,0 +1,42 @@
+/**
+ * @param {string} word
+ * @param {string} abbr
+ * @return {boolean}
+ */
+var validWordAbbreviation = function(word, abbr) {
+  if (abbr.length > word.length) {
+    return false;
+  }
+
+  let i = 0,
+    j = 0,
+    number = 0;
+  while (i < word.length) {
+    let c = parseInt(abbr[j]);
+
+    if (isNaN(c)) {
+      if (word[i] !== abbr[j]) {
+        return false;
+      } else {
+        i++;
+        j++;
+      }
+    } else {
+      const next = parseInt(abbr[j + 1]);
+      if (!isNaN(next)) {
+        if (c === 0 && next > 0) {
+          return false;
+        }
+
+        c = 10 * c + next;
+        j += 2;
+      } else {
+        j += 1;
+      }
+
+      i += c;
+    }
+  }
+
+  return i === word.length && j === abbr.length;
+};
