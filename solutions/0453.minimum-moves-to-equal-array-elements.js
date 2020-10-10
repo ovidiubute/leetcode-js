8827949f56fb0ee0e9d4050fb3d85a641510d99e
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-10-10 15:23:21 +0000

    Add solution to 453.

---
+++ solutions/0453.minimum-moves-to-equal-array-elements.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minMoves = function(nums) {
+  let s = 0;
+  for (let i = 0; i < nums.length; i++) {
+    s += nums[i];
+  }
+
+  return s - nums.length * Math.min(...nums);
+};
