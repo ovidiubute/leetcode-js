b774d1a0d80498765f7b52b1af1fcdbf02910001
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-10-11 13:07:24 +0000

    Add solution to 1572.

---
+++ solutions/1572.matrix-diagonal-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} mat
+ * @return {number}
+ */
+var diagonalSum = function(mat) {
+  let s = 0;
+  
+  for (let i = 0; i < mat.length; i++) {
+    s += mat[i][i];
+    s += mat[i][mat.length - i - 1];
+  }
+  
+  if (mat.length % 2 === 1) {
+    const mid = Math.floor(mat.length / 2);
+    s -= mat[mid][mid]
+  }
+  
+  
+  return s;
+};
