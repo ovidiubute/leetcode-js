fad58ed033013d6881646602eb393703e01c2858
AutoBot <AutoBot@users.noreply.github.com>
2020-10-11 13:07:47 +0000

    Automated badge update.

--- solutions/1572.matrix-diagonal-sum.js
+++ solutions/1572.matrix-diagonal-sum.js
@@ -2,19 +2,18 @@
  * @param {number[][]} mat
  * @return {number}
  */
-var diagonalSum = function(mat) {
+var diagonalSum = function (mat) {
   let s = 0;
-  
+
   for (let i = 0; i < mat.length; i++) {
     s += mat[i][i];
     s += mat[i][mat.length - i - 1];
   }
-  
+
   if (mat.length % 2 === 1) {
     const mid = Math.floor(mat.length / 2);
-    s -= mat[mid][mid]
+    s -= mat[mid][mid];
   }
-  
-  
+
   return s;
 };
