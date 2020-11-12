76e50931a34244f7a23ce5a077f32dae0232ca02
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-11-12 16:30:18 +0000

    Update problem count.

--- update-badge.js
+++ update-badge.js
@@ -1,5 +1,5 @@
 // Update this over time from leetcode.com
-const TOTAL_PROBLEM_COUNT = 1501;
+const TOTAL_PROBLEM_COUNT = 1502;
 
 // Count how many solutions we have so far
 const countSolutionsScript = "ls solutions | wc -l";
