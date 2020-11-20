f8bdad9e872b84b78b96301f2775383e0fd011dd
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-11-20 12:46:14 +0000

    Shorter 796 solution.

--- solutions/0796.rotate-string.js
+++ solutions/0796.rotate-string.js
@@ -4,24 +4,5 @@
  * @return {boolean}
  */
 var rotateString = function (A, B) {
-  if (A === "" && B === "") {
-    return true;
-  }
-
-  if (A.length !== B.length) {
-    return false;
-  }
-
-  let i = 0;
-  while (i < A.length) {
-    if (A === B) {
-      return true;
-    }
-
-    A = A.slice(1) + A[0];
-
-    i++;
-  }
-
-  return false;
+  return A.length === B.length && (A + A).includes(B);
 };
