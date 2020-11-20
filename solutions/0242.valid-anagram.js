fda9bb51aef60857dfb5484888027eb5a8fc7d81
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-11-20 11:25:42 +0000

    Improve 242.

--- solutions/0242.valid-anagram.js
+++ solutions/0242.valid-anagram.js
@@ -4,11 +4,27 @@
  * @return {boolean}
  */
 var isAnagram = function (s, t) {
-  let x = s.split("");
-  x.sort();
+  if (s.length !== t.length) {
+    return false;
+  }
 
-  let y = t.split("");
-  y.sort();
+  let counter = [];
+  const aCode = "a".charCodeAt(0);
 
-  return x.join("") === y.join("");
+  for (let i = 0; i < 26; i++) {
+    counter[i] = 0;
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    counter[s[i].charCodeAt(0) - aCode]++;
+    counter[t[i].charCodeAt(0) - aCode]--;
+  }
+
+  for (let i = 0; i < 26; i++) {
+    if (counter[i] !== 0) {
+      return false;
+    }
+  }
+
+  return true;
 };
