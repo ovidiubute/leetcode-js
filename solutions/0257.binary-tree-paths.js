db5cc2257a5482cf6891ed49717b69bab85a95c2
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-11-21 20:50:17 +0000

    Add solution to 257.

---
+++ solutions/0257.binary-tree-paths.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function (root) {
+  const solutions = [];
+
+  dfs(root, "", solutions);
+
+  return solutions;
+};
+
+var dfs = function (node, currPath, solutions) {
+  if (node === null) {
+    return;
+  }
+
+  currPath += node.val;
+  if (!node.left && !node.right) {
+    solutions.push(currPath);
+  } else {
+    currPath += "->";
+    dfs(node.left, currPath, solutions);
+    dfs(node.right, currPath, solutions);
+  }
+};
