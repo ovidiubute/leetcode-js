30180285f8a3dabf7b0bf8fadf48dd9ed4f45777
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-11-21 15:56:55 +0000

    Update 131 solution (easier to read).

--- solutions/0131.palindrome-partitioning.js
+++ solutions/0131.palindrome-partitioning.js
@@ -3,30 +3,29 @@
  * @return {string[][]}
  */
 var partition = function (s) {
-  const result = [];
-
-  partitionAt(s, 0, "", result);
-
-  return result;
+  const results = [];
+  dfs(s, 0, [], results);
+  return results;
 };
 
-const partitionAt = function (s, offset, prefix, result) {
-  if (s.length <= offset) {
-    result.push(prefix.split(",").slice(1));
+var dfs = function (s, start, set, results) {
+  if (start >= s.length) {
+    results.push(Array.from(set));
     return;
   }
 
-  for (let i = offset; i < s.length; i++) {
-    if (isPalindrome(s, offset, i)) {
-      const substr = s.substring(offset, i + 1);
-      partitionAt(s, i + 1, prefix + "," + substr, result);
+  for (let end = start; end < s.length; end++) {
+    if (isPalindrome(s, start, end)) {
+      set.push(s.slice(start, end + 1));
+      dfs(s, end + 1, set, results);
+      set.pop();
     }
   }
 };
 
-const isPalindrome = function (s, start, end) {
+const isPalindrome = (s, start, end) => {
   while (start < end) {
-    if (s.charAt(start++) !== s.charAt(end--)) {
+    if (s[start++] !== s[end--]) {
       return false;
     }
   }
