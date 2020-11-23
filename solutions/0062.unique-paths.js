da23980ddaa9cd7b490d0019888a55db966c9568
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-11-23 18:03:50 +0000

    Add solution to 62.

---
+++ solutions/0062.unique-paths.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function (m, n) {
+  let dp = [];
+  for (let i = 0; i < m; i++) {
+    dp[i] = [];
+    for (let j = 0; j < n; j++) {
+      dp[i][j] = 0;
+    }
+  }
+
+  for (let i = 0; i < m; i++) {
+    dp[i][0] = 1;
+  }
+
+  for (let j = 0; j < n; j++) {
+    dp[0][j] = 1;
+  }
+
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    }
+  }
+
+  return dp[m - 1][n - 1];
+};
