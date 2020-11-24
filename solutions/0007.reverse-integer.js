b7a1b195d9ed207870e7fcf75c91d0d69c03d8ca
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-11-24 20:41:58 +0000

    Update 7.

--- solutions/0007.reverse-integer.js
+++ solutions/0007.reverse-integer.js
@@ -3,22 +3,20 @@
  * @return {number}
  */
 var reverse = function (x) {
-  let temp = Math.abs(x);
   let rev = 0;
+  let negative = x < 0;
 
-  while (temp !== 0) {
-    let c = temp % 10;
-    rev = rev * 10 + c;
-    temp = Math.floor(temp / 10);
+  x = Math.abs(x);
+
+  while (x) {
+    rev = rev * 10 + (x % 10);
+
+    if (rev >= Math.pow(2, 31)) {
+      return 0;
+    }
+
+    x = Math.floor(x / 10);
   }
 
-  if (rev > Math.pow(2, 31)) {
-    return 0;
-  }
-
-  if (x < 0) {
-    return 0 - rev;
-  }
-
-  return rev;
+  return negative ? 0 - rev : rev;
 };
