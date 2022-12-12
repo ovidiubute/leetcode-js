7fca4b48ffa276666a385512849d65218b04fce1
Ovidiu Bute <ovidiu.bute@gmail.com>
2022-12-13 00:20:50 +0200

    Add solution to 36.

---
+++ solutions/0036.valid-sudoku.js
@@ -0,0 +1,82 @@
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function (board) {
+  const n = board.length;
+
+  for (let i = 0; i < n; i++) {
+    const lines = [];
+    const columns = [];
+    for (let j = 0; j < n; j++) {
+      // Line by line
+      lines.push(board[i][j]);
+
+      // Column by column
+      columns.push(board[j][i]);
+    }
+
+    let result = hasDuplicates(lines);
+    if (result) {
+      return false;
+    }
+
+    result = hasDuplicates(columns);
+    if (result) {
+      return false;
+    }
+  }
+
+  // 3 by 3 sums
+  for (let i = 0; i < 9; i += 3) {
+    for (let j = 0; j < 9; j += 3) {
+      const square = [];
+      for (let x = i; x < i + 3; x++) {
+        for (let y = j; y < j + 3; y++) {
+          square.push(board[x][y]);
+        }
+      }
+
+      result = hasDuplicates(square);
+      if (result) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+};
+
+const hasDuplicates = (entries) => {
+  const seen = {};
+
+  for (let i = 0; i < entries.length; i++) {
+    if (entries[i] === ".") {
+      continue;
+    }
+
+    if (seen[entries[i]]) {
+      return true;
+    }
+
+    seen[entries[i]] = true;
+  }
+
+  return false;
+};
+
+const b = [
+  ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+  ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+  [".", "9", "8", ".", ".", ".", ".", "6", "."],
+  ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+  ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+  ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+  [".", "6", ".", ".", ".", ".", "2", "8", "."],
+  [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+  [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+];
+
+const r = isValidSudoku(b);
+
+console.log(r);
