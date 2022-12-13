11a5a710e423b2c213566ad2c0f32372a161cc85
Ovidiu Bute <ovidiu.bute@gmail.com>
2022-12-13 10:49:19 +0200

    Add easier to follow solution for 34.

--- solutions/0034.find-first-and-last-position-of-element-in-sorted-array.js
+++ solutions/0034.find-first-and-last-position-of-element-in-sorted-array.js
@@ -4,33 +4,47 @@
  * @return {number[]}
  */
 var searchRange = function (nums, target) {
-  let firstMatch = binarySearch(nums, 0, nums.length - 1, target);
+  const out = [-1, -1];
 
-  if (firstMatch === -1) return [-1, -1];
+  out[0] = getFirst(nums, target);
+  out[1] = getLast(nums, target);
 
-  let i = firstMatch,
-    j = firstMatch;
-  while (i > -1 && nums[i - 1] === target) {
-    i--;
-  }
-  while (j < nums.length - 1 && nums[j + 1] === target) {
-    j++;
-  }
-
-  return [i, j];
+  return out;
 };
 
-const binarySearch = function (nums, left, right, target) {
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    if (nums[mid] === target) {
-      return mid;
-    }
+var getFirst = function (nums, target) {
+  var lo = 0,
+    hi = nums.length - 1;
+  while (lo <= hi) {
+    let mid = Math.floor((lo + hi) / 2);
 
-    if (nums[mid] < target) {
-      left = mid + 1;
+    if (target === nums[mid] && (mid === 0 || nums[mid - 1] < target)) {
+      return mid;
+    } else if (target > nums[mid]) {
+      lo = mid + 1;
     } else {
-      right = mid - 1;
+      hi = mid - 1;
+    }
+  }
+
+  return -1;
+};
+
+var getLast = function (nums, target) {
+  var lo = 0,
+    hi = nums.length - 1;
+  while (lo <= hi) {
+    let mid = Math.floor((lo + hi) / 2);
+
+    if (
+      target === nums[mid] &&
+      (mid === nums.length - 1 || nums[mid + 1] > target)
+    ) {
+      return mid;
+    } else if (target < nums[mid]) {
+      hi = mid - 1;
+    } else {
+      lo = mid + 1;
     }
   }
 
