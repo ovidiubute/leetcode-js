f9eadc742014723cb3e688e6e46fac3f75b4cacf
Ovidiu Bute <ovidiu.bute@gmail.com>
2022-12-21 15:40:58 +0200

    Implement 153 in O(log(n)).

--- solutions/0153.find-minimum-in-rotated-sorted-array.js
+++ solutions/0153.find-minimum-in-rotated-sorted-array.js
@@ -3,11 +3,37 @@
  * @return {number}
  */
 var findMin = function (nums) {
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i - 1] > nums[i]) {
-      return nums[i];
-    }
+  const n = nums.length;
+
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return nums[0];
+  } else if (nums[0] < nums[n - 1]) {
+    return nums[0];
   }
 
-  return nums[0];
+  let lo = 0,
+    hi = n - 1;
+
+  while (lo <= hi) {
+    const mid = Math.floor((lo + hi) / 2);
+
+    if (nums[mid] > nums[mid + 1]) {
+      return nums[mid + 1];
+    }
+
+    if (nums[mid - 1] > nums[mid]) {
+      return nums[mid];
+    }
+
+    if (nums[mid] < nums[0]) {
+      hi = mid - 1;
+    } else {
+      lo = mid + 1;
+    }
+  }
 };
+
+const nums = [3, 4, 5, 1, 2];
+findMin(nums);
