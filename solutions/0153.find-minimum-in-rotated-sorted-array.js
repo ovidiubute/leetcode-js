2132fccea3fb0ce7b13093357b1c0d93cbc8f470
Ovidiu Bute <ovidiu.bute@gmail.com>
2022-12-23 09:47:31 +0200

    Add easier to follow solution for 153.

--- solutions/0153.find-minimum-in-rotated-sorted-array.js
+++ solutions/0153.find-minimum-in-rotated-sorted-array.js
@@ -1,39 +1,33 @@
 /**
  * @param {number[]} nums
+ * @param {number} target
  * @return {number}
  */
-var findMin = function (nums) {
-  const n = nums.length;
+var search = function (nums, target) {
+  let start = 0,
+    end = nums.length - 1;
 
-  if (n === 0) {
-    return 0;
-  } else if (n === 1) {
-    return nums[0];
-  } else if (nums[0] < nums[n - 1]) {
-    return nums[0];
-  }
+  while (start <= end) {
+    let mid = Math.floor((end + start) / 2);
 
-  let lo = 0,
-    hi = n - 1;
-
-  while (lo <= hi) {
-    const mid = Math.floor((lo + hi) / 2);
-
-    if (nums[mid] > nums[mid + 1]) {
-      return nums[mid + 1];
+    if (target === nums[mid]) {
+      return mid;
     }
 
-    if (nums[mid - 1] > nums[mid]) {
-      return nums[mid];
-    }
-
-    if (nums[mid] < nums[0]) {
-      hi = mid - 1;
+    if (nums[mid] >= nums[start]) {
+      if (target >= nums[start] && target < nums[mid]) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
     } else {
-      lo = mid + 1;
+      if (target > nums[mid] && target <= nums[end]) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
     }
   }
-};
 
-const nums = [3, 4, 5, 1, 2];
-findMin(nums);
+  return -1;
+};
