f22133f74740d217197f79efa8dee8ac2ef2b8a3
Ovidiu Bute <ovidiu.bute@gmail.com>
2023-01-14 08:46:45 +0200

    Use sets in solution 73.

--- solutions/0073.set-matrix-zeroes.js
+++ solutions/0073.set-matrix-zeroes.js
@@ -3,21 +3,21 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function (matrix) {
-  const zLines = {};
-  const zColumns = {};
+  const zeroRows = new Set();
+  const zeroCols = new Set();
 
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
       if (matrix[i][j] === 0) {
-        zLines[i] = 1;
-        zColumns[j] = 1;
+        zeroRows.add(i);
+        zeroCols.add(j);
       }
     }
   }
 
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
-      if (zLines[i] || zColumns[j]) {
+      if (zeroRows.has(i) || zeroCols.has(j)) {
         matrix[i][j] = 0;
       }
     }
