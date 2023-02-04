ec94e20913905273b1f96aac881e506c9055cd9e
Ovidiu Bute <ovidiu.bute@gmail.com>
2023-02-04 15:01:25 +0200

    Improve solution for 170.

--- solutions/0170.two-sum-iii-data-structure-design.js
+++ solutions/0170.two-sum-iii-data-structure-design.js
@@ -1,38 +1,46 @@
-/**
- * Initialize your data structure here.
- */
 var TwoSum = function () {
-  this.numbers = [];
-  this.sums = new Map();
+  this.map = new Map();
 };
 
 /**
- * Add the number to an internal data structure..
  * @param {number} number
  * @return {void}
  */
 TwoSum.prototype.add = function (number) {
-  for (let i = 0; i < this.numbers.length; i++) {
-    const s = this.numbers[i] + number;
-
-    this.sums.set(s, true);
+  if (this.map.has(number)) {
+    this.map.set(number, this.map.get(number) + 1);
+  } else {
+    this.map.set(number, 1);
   }
-
-  this.numbers.push(number);
 };
 
 /**
- * Find if there exists any pair of numbers which sum is equal to the value.
  * @param {number} value
  * @return {boolean}
  */
 TwoSum.prototype.find = function (value) {
-  return this.sums.has(value) ? this.sums.get(value) : false;
+  const keys = this.map.keys();
+  for (key of keys) {
+    const current = this.map.get(key);
+    const complement = value - key;
+
+    if (complement === key) {
+      if (current > 1) {
+        return true;
+      }
+    } else {
+      if (this.map.has(complement)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
 };
 
 /**
  * Your TwoSum object will be instantiated and called as such:
- * var obj = Object.create(TwoSum).createNew()
+ * var obj = new TwoSum()
  * obj.add(number)
  * var param_2 = obj.find(value)
  */
